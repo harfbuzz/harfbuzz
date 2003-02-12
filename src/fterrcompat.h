5febce1ffc956ea44526d50be7cf14c69c66cb34
Owen Taylor <otaylor@redhat.com>
2003-02-12 22:12:55 +0000

    Up the FreeType version requirement to 2.0.9. (2.2.1 had a compilation
    Wed Feb 12 16:59:23 2003  Owen Taylor  <otaylor@redhat.com>
    
            * configure.in pango/opentype/fterrcompat.h README:
            Up the FreeType version requirement to 2.0.9.
            (2.2.1 had a compilation failure with older versions,
            this catches it more cleanly, and removes some unneeded
            checks. #105302, problem reported by Will Partain.)

--- src/fterrcompat.h
+++ src/fterrcompat.h
@@ -4,14 +4,10 @@
 
 #include <config.h>
 
-#ifdef HAVE_FREETYPE_2_0_3
-#include <freetype/internal/tterrors.h>
-#else
 #define TT_Err_Ok FT_Err_Ok
 #define TT_Err_Invalid_Argument FT_Err_Invalid_Argument
 #define TT_Err_Invalid_Face_Handle FT_Err_Invalid_Face_Handle
 #define TT_Err_Table_Missing FT_Err_Table_Missing
-#endif /* HAVE_FREETYPE_2_0_3 */
 
 /* Compat macros for name changes in FreeType 2.1.0
  */
