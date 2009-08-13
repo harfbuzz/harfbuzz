12eae6d55d468ff2fbe2bc12bec188d313c671f9
Behdad Esfahbod <behdad@behdad.org>
2009-08-13 05:20:46 -0400

    [HB] Fix buffer sizing issue with mingw gcc
    Bit field packing is a delicate field.  Issue reported in comment 3 here:
    http://bugzilla.gnome.org/show_bug.cgi?id=591511

--- src/hb-buffer-private.h
+++ src/hb-buffer-private.h
@@ -50,15 +50,15 @@ typedef struct _hb_internal_glyph_position_t {
   hb_position_t  y_pos;
   hb_position_t  x_advance;
   hb_position_t  y_advance;
-  hb_bool_t      new_advance :1;	/* if set, the advance width values are
-					   absolute, i.e., they won't be
-					   added to the original glyph's value
-					   but rather replace them */
+  unsigned short new_advance :1;	/* if set, the advance width values are
+					 * absolute, i.e., they won't be
+					 * added to the original glyph's value
+					 * but rather replace them */
   unsigned short back : 15;		/* number of glyphs to go back
-					   for drawing current glyph */
+					 * for drawing current glyph */
   short          cursive_chain : 16;	/* character to which this connects,
-					   may be positive or negative; used
-					   only internally */
+					 * may be positive or negative; used
+					 * only internally */
 } hb_internal_glyph_position_t;
 
 ASSERT_STATIC (sizeof (hb_glyph_info_t) == sizeof (hb_internal_glyph_info_t));
