3111b8a0d5b38da57c0f6285aec2b92eb690188f
Behdad Esfahbod <behdad@behdad.org>
2010-10-13 10:41:53 -0400

    Fix stupid bug, oops

--- src/hb-font.cc
+++ src/hb-font.cc
@@ -144,7 +144,7 @@ hb_font_funcs_make_immutable (hb_font_funcs_t *ffuncs)
 hb_bool_t
 hb_font_funcs_is_immutable (hb_font_funcs_t *ffuncs)
 {
-  return ffuncs->immutable = TRUE;
+  return ffuncs->immutable;
 }
 
 
