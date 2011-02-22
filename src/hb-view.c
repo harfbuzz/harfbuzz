5353bf439c150492708ef9337078cfd73b83627b
Behdad Esfahbod <behdad@behdad.org>
2011-02-22 18:06:19 -0500

    Add initial hb-view tool
    Currently doesn't handle script or direction.

---
+++ src/hb-view.c
@@ -0,0 +1,342 @@
+/*
+ * Copyright (C) 2010  Behdad Esfahbod
+ * Copyright (C) 2011  Google, Inc.
+ *
+ *  This is part of HarfBuzz, a text shaping library.
+ *
+ * Permission is hereby granted, without written agreement and without
+ * license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided that the
+ * above copyright notice and the following two paragraphs appear in
+ * all copies of this software.
+ *
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN
+ * IF THE COPYRIGHT HOLDER HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ * THE COPYRIGHT HOLDER SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
+ * ON AN "AS IS" BASIS, AND THE COPYRIGHT HOLDER HAS NO OBLIGATION TO
+ * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * Google Author(s): Behdad Esfahbod
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include <glib.h>
+#include <hb-glib.h>
+
+#include <cairo-ft.h>
+#include <hb-ft.h>
+
+/* Controlled by cmd-line options */
+static int margin_t = 10;
+static int margin_b = 10;
+static int margin_l = 10;
+static int margin_r = 10;
+static int line_space = 0;
+static double font_size = 18;
+static const char *fore = "#000000";
+static const char *back = "#000000";
+static const char *text = NULL;
+static const char *font_file = NULL;
+static const char *out_file = "/dev/stdout";
+static const char *language = NULL;
+
+/* Ugh, global vars.  Ugly, but does the job */
+static int width = 0;
+static int height = 0;
+static cairo_surface_t *surface = NULL;
+static cairo_pattern_t *fore_pattern = NULL;
+static cairo_pattern_t *back_pattern = NULL;
+static FT_Library ft_library;
+static FT_Face ft_face;
+static cairo_font_face_t *cairo_face;
+
+
+G_GNUC_NORETURN static void
+usage (FILE *f, int status)
+{
+  fprintf (f, "usage: hb-view [OPTS...] font-file text\n");
+  exit (status);
+}
+
+G_GNUC_NORETURN static void
+version (void)
+{
+  printf ("hb-view (harfbuzz) %s\n", PACKAGE_VERSION);
+  exit (0);
+}
+
+static void
+parse_opts (int argc, char **argv)
+{
+  argv[0] = (char *) "hb-view";
+  while (1)
+    {
+      int option_index = 0, c;
+      static struct option long_options[] = {
+	{"help", 0, 0, 'h'},
+	{"version", 0, 0, 'v'},
+	{"margin", 1, 0, 'm'},
+	{"line-space", 1, 0, 'l'},
+	{"font-size", 1, 0, 's'},
+	{"foreground", 1, 0, 'F'},
+	{"background", 1, 0, 'B'},
+	{"language", 1, 0, 'L'},
+	{"output", 1, 0, 'o'},
+	{0, 0, 0, 0}
+      };
+
+      c = getopt_long (argc, argv, "", long_options, &option_index);
+      if (c == -1)
+	break;
+
+      switch (c)
+	{
+	case 0:
+	  break;
+	case 'h':
+	  usage (stdout, 0);
+	  break;
+	case 'v':
+	  version ();
+	  break;
+	case 'l':
+	  line_space = atoi (optarg);
+	  break;
+	case 'm':
+	  switch (sscanf (optarg, "%d %d %d %d", &margin_t, &margin_r, &margin_b, &margin_l)) {
+	    case 1: margin_r = margin_t;
+	    case 2: margin_b = margin_t;
+	    case 3: margin_l = margin_r;
+	  }
+	  break;
+	case 's':
+	  font_size = strtod (optarg, NULL);
+	  break;
+	case 'f':
+	  font_file = optarg;
+	  break;
+	case 'F':
+	  fore = optarg;
+	  break;
+	case 'B':
+	  back = optarg;
+	  break;
+	case 't':
+	  text = optarg;
+	  break;
+	case 'L':
+	  language = optarg;
+	  break;
+	case 'o':
+	  out_file = optarg;
+	  break;
+	case '?':
+	  usage (stdout, 1);
+	  break;
+	default:
+	  break;
+	}
+    }
+  if (optind + 2 != argc)
+    usage (stderr, 1);
+  font_file = argv[optind++];
+  text = argv[optind++];
+}
+
+
+
+static cairo_glyph_t *
+_hb_cr_text_glyphs (cairo_t *cr,
+		    const char *text, int len,
+		    unsigned int *pnum_glyphs)
+{
+  cairo_scaled_font_t *scaled_font = cairo_get_scaled_font (cr);
+  FT_Face ft_face = cairo_ft_scaled_font_lock_face (scaled_font);
+  hb_face_t *hb_face = hb_ft_face_create (ft_face, NULL);
+  hb_font_t *hb_font = hb_ft_font_create (ft_face, NULL);
+  hb_buffer_t *hb_buffer;
+  cairo_glyph_t *cairo_glyphs;
+  hb_glyph_info_t *hb_glyph;
+  hb_glyph_position_t *hb_position;
+  unsigned int num_glyphs, i;
+  hb_position_t x;
+
+  if (len < 0)
+    len = strlen (text);
+  hb_buffer = hb_buffer_create (len);
+
+  hb_buffer_set_unicode_funcs (hb_buffer, hb_glib_get_unicode_funcs ());
+
+  hb_buffer_add_utf8 (hb_buffer, text, len, 0, len);
+  /* hb_buffer_set_script (hb_buffer, HB_SCRIPT_LATIN); */
+  /* hb_buffer_set_direction (hb_buffer, HB_DIRECTION_LTR); */
+  hb_buffer_set_language (hb_buffer, hb_language_from_string (language));
+
+  hb_shape (hb_font, hb_face, hb_buffer, NULL, 0);
+
+  num_glyphs = hb_buffer_get_length (hb_buffer);
+  hb_glyph = hb_buffer_get_glyph_infos (hb_buffer);
+  hb_position = hb_buffer_get_glyph_positions (hb_buffer);
+  cairo_glyphs = cairo_glyph_allocate (num_glyphs + 1);
+  x = 0;
+  for (i = 0; i < num_glyphs; i++)
+    {
+      cairo_glyphs[i].index = hb_glyph->codepoint;
+      cairo_glyphs[i].x = (hb_position->x_offset + x) * (1./64);
+      cairo_glyphs[i].y = -(hb_position->y_offset)    * (1./64);
+      x += hb_position->x_advance;
+
+      hb_glyph++;
+      hb_position++;
+    }
+  cairo_glyphs[i].x = x * (1./64);
+  hb_buffer_destroy (hb_buffer);
+  hb_font_destroy (hb_font);
+  hb_face_destroy (hb_face);
+  cairo_ft_scaled_font_unlock_face (scaled_font);
+
+  if (pnum_glyphs)
+    *pnum_glyphs = num_glyphs;
+  return cairo_glyphs;
+}
+
+static cairo_t *
+create_context (void)
+{
+  cairo_t *cr;
+  unsigned int fr, fg, fb, fa, br, bg, bb, ba;
+
+  if (surface)
+    cairo_surface_destroy (surface);
+  if (back_pattern)
+    cairo_pattern_destroy (back_pattern);
+  if (fore_pattern)
+    cairo_pattern_destroy (fore_pattern);
+
+  br = bg = bb = ba = 255;
+  sscanf (back + (*back=='#'), "%2x%2x%2x%2x", &br, &bg, &bb, &ba);
+  fr = fg = fb = 0; fa = 255;
+  sscanf (fore + (*fore=='#'), "%2x%2x%2x%2x", &fr, &fg, &fb, &fa);
+
+  if (ba == 255 && fa == 255 && br == bg && bg == bb && fr == fg && fg == fb) {
+    /* grayscale.  use A8 surface */
+    surface = cairo_image_surface_create (CAIRO_FORMAT_A8, width, height);
+    cr = cairo_create (surface);
+    cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
+    cairo_set_source_rgba (cr, 1., 1., 1., br / 255.);
+    cairo_paint (cr);
+    back_pattern = cairo_pattern_reference (cairo_get_source (cr));
+    cairo_set_source_rgba (cr, 1., 1., 1., fr / 255.);
+    fore_pattern = cairo_pattern_reference (cairo_get_source (cr));
+  } else {
+    /* color.  use (A)RGB surface */
+    if (ba != 255)
+      surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, width, height);
+    else
+      surface = cairo_image_surface_create (CAIRO_FORMAT_RGB24, width, height);
+    cr = cairo_create (surface);
+    cairo_set_source_rgba (cr, br / 255., bg / 255., bb / 255., ba / 255.);
+    cairo_paint (cr);
+    back_pattern = cairo_pattern_reference (cairo_get_source (cr));
+    cairo_set_source_rgba (cr, fr / 255., fg / 255., fb / 255., fa / 255.);
+    fore_pattern = cairo_pattern_reference (cairo_get_source (cr));
+  }
+
+  cairo_set_font_face (cr, cairo_face);
+
+  return cr;
+}
+
+static void
+draw (void)
+{
+  cairo_t *cr;
+  cairo_font_extents_t font_extents;
+
+  cairo_glyph_t *glyphs = NULL;
+  unsigned int num_glyphs = 0;
+
+  const char *end, *p = text;
+  double x, y;
+
+  cr= create_context ();
+
+  cairo_set_font_size (cr, font_size);
+  cairo_font_extents (cr, &font_extents);
+
+  height = 0;
+  width = 0;
+
+  x = margin_l;
+  y = margin_t;
+
+  do {
+    cairo_text_extents_t extents;
+
+    end = strchr (p, '\n');
+    if (!end)
+      end = p + strlen (p);
+
+    if (p != text)
+	y += line_space;
+
+    if (p != end) {
+      glyphs = _hb_cr_text_glyphs (cr, p, end - p, &num_glyphs);
+      cairo_glyph_extents (cr, glyphs, num_glyphs, &extents);
+
+      y += ceil (font_extents.ascent);
+      width = MAX (width, extents.x_advance);
+      cairo_save (cr);
+      cairo_translate (cr, x, y);
+      cairo_show_glyphs (cr, glyphs, num_glyphs);
+      cairo_restore (cr);
+      y += ceil (font_extents.height - ceil (font_extents.ascent));
+      cairo_glyph_free (glyphs);
+    }
+
+    p = end + 1;
+  } while (*end);
+
+  height = y + margin_b;
+  width += margin_l + margin_r;
+
+  cairo_destroy (cr);
+}
+
+
+
+int
+main (int argc, char **argv)
+{
+  parse_opts (argc, argv);
+
+  FT_Init_FreeType (&ft_library);
+  if (FT_New_Face (ft_library, font_file, 0, &ft_face)) {
+    fprintf (stderr, "Failed to open font file `%s'\n", font_file);
+    exit (1);
+  }
+  cairo_face = cairo_ft_font_face_create_for_ft_face (ft_face, 0);
+
+  draw ();
+  draw ();
+
+  cairo_surface_write_to_png (surface, out_file);
+
+  return 0;
+}
