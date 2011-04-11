d77b76200efbaa0611691920f9f2018b1e8be340
Behdad Esfahbod <behdad@behdad.org>
2011-04-11 12:29:52 -0400

    Add few more paratheses to the HB_TAG macro
    Never hurts.

--- src/hb-common.h
+++ src/hb-common.h
@@ -54,7 +54,7 @@ typedef unsigned long long uint64_t;
 typedef int hb_bool_t;
 
 typedef uint32_t hb_tag_t;
-#define HB_TAG(a,b,c,d) ((hb_tag_t)(((uint8_t)a<<24)|((uint8_t)b<<16)|((uint8_t)c<<8)|(uint8_t)d))
+#define HB_TAG(a,b,c,d) ((hb_tag_t)((((uint8_t)(a))<<24)|(((uint8_t)(b))<<16)|(((uint8_t)(c))<<8)|((uint8_t)(d))))
 #define HB_TAG_STR(s)   (HB_TAG(((const char *) s)[0], \
 				((const char *) s)[1], \
 				((const char *) s)[2], \
