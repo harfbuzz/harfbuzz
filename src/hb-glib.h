0809aadd4bbd5d0f256407def7cc10b79772a824
Behdad Esfahbod <behdad@behdad.org>
2011-04-20 02:44:29 -0400

    [glib] Add two-way script conversion functions
    Also optimizes the common-direction script lookup to be an array lookup.

--- src/hb-glib.h
+++ src/hb-glib.h
@@ -28,8 +28,16 @@
 #define HB_GLIB_H
 
 #include "hb.h"
+#include <glib.h>
 
 HB_BEGIN_DECLS
+
+
+hb_script_t
+hb_glib_script_to_script (GUnicodeScript script);
+
+GUnicodeScript
+hb_glib_script_from_script (hb_script_t script);
 
 
 hb_unicode_funcs_t *
