e3693b72f0651985d4f619cde668611639dca885
Behdad Esfahbod <behdad@behdad.org>
2011-06-15 09:33:52 -0400

    Change a couple strstr() to strchr()

--- src/hb-ot-tag.cc
+++ src/hb-ot-tag.cc
@@ -586,10 +586,10 @@ lang_compare_first_component (const char *a,
   unsigned int da, db;
   const char *p;
 
-  p = strstr (a, "-");
+  p = strchr (a, '-');
   da = p ? (unsigned int) (p - a) : strlen (a);
 
-  p = strstr (b, "-");
+  p = strchr (b, '-');
   db = p ? (unsigned int) (p - b) : strlen (b);
 
   return strncmp (a, b, MAX (da, db));
