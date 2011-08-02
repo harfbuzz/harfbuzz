b301478a69d961c724a4875b839a81fb458d1153
Behdad Esfahbod <behdad@behdad.org>
2011-08-02 11:25:13 -0400

    Bug 39763 - autogen.sh should check pkg-config availability
    (and revert change have_icu change I mistakenly pushed out)

--- autogen.sh
+++ autogen.sh
@@ -13,14 +13,20 @@ which ragel || {
 	exit 1
 }
 
+echo -n "checking for pkg-config... "
+which pkg-config || {
+	echo "*** No pkg-config found, please install it ***"
+	exit 1
+}
+
 echo -n "checking for autoreconf... "
 which autoreconf || {
 	echo "*** No autoreconf found, please install it ***"
 	exit 1
 }
 
-echo "running autoreconf"
-autoreconf --force --install || exit $?
+echo "running autoreconf --force --install --verbose"
+autoreconf --force --install --verbose || exit $?
 
 cd $olddir
 echo "running configure $@"
