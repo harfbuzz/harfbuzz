c747f509bcc4e6a34ced04e9e79ed414a44437b1
Behdad Esfahbod <behdad@behdad.org>
2011-08-04 01:51:30 -0400

    More out-of-tree build fixes

--- src/check-c-linkage-decls.sh
+++ src/check-c-linkage-decls.sh
@@ -10,9 +10,8 @@ test "x$HBHEADERS" = x && HBHEADERS=`find . -maxdepth 1 -name 'hb*.h'`
 test "x$HBSOURCES" = x && HBSOURCES=`find . -maxdepth 1 -name 'hb-*.cc' -or -name 'hb-*.hh'`
 
 
-cd "$srcdir"
-
 for x in $HBHEADERS $HBSOURCES; do
+	test -f $srcdir/$x && x=$srcdir/$x
 	if ! grep -q HB_BEGIN_DECLS "$x" || ! grep -q HB_END_DECLS "$x"; then
 		echo "Ouch, file $x does not HB_BEGIN_DECLS / HB_END_DECLS"
 		stat=1
--- src/check-header-guards.sh
+++ src/check-header-guards.sh
@@ -10,12 +10,12 @@ test "x$HBHEADERS" = x && HBHEADERS=`find . -maxdepth 1 -name 'hb*.h'`
 test "x$HBSOURCES" = x && HBSOURCES=`find . -maxdepth 1 -name 'hb-*.cc' -or -name 'hb-*.hh'`
 
 
-cd "$srcdir"
-
+echo  $srcdir
 for x in $HBHEADERS $HBSOURCES; do
+	test -f "$srcdir/$x" && x="$srcdir/$x"
 	echo "$x" | grep '[^h]$' -q && continue;
-	x=`echo "$x" | sed 's@.*/@@'`
-	tag=`echo "$x" | tr 'a-z.-' 'A-Z_'`
+	xx=`echo "$x" | sed 's@.*/@@'`
+	tag=`echo "$xx" | tr 'a-z.-' 'A-Z_'`
 	lines=`grep "\<$tag\>" "$x" | wc -l`
 	if test "x$lines" != x3; then
 		echo "Ouch, header file $x does not have correct preprocessor guards"
