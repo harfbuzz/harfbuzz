210a06f3d7bd2df55ebd1743da74f327c5a7a967
Behdad Esfahbod <behdad@behdad.org>
2011-08-26 13:39:49 +0200

    Minor

--- src/hb-graphite2.h
+++ src/hb-graphite2.h
@@ -26,7 +26,6 @@
 #ifndef HB_GRAPHITE2_H
 #define HB_GRAPHITE2_H
 
-#include "hb.h"
 #include "hb-shape.h"
 
 HB_BEGIN_DECLS
@@ -34,11 +33,12 @@ HB_BEGIN_DECLS
 
 #define HB_GRAPHITE_TAG_Silf HB_TAG('S','i','l','f')
 
-hb_bool_t hb_graphite_shape (hb_font_t    *font,
-			hb_buffer_t  *buffer,
-			const hb_feature_t *features,
-			unsigned int  num_features,
-            const char * const *shaper_options);
+hb_bool_t
+hb_graphite_shape (hb_font_t          *font,
+		   hb_buffer_t        *buffer,
+		   const hb_feature_t *features,
+		   unsigned int        num_features,
+		   const char * const *shaper_options);
 
 HB_END_DECLS
 
--- src/hb-ot-shape.h
+++ src/hb-ot-shape.h
@@ -27,7 +27,6 @@
 #ifndef HB_OT_SHAPE_H
 #define HB_OT_SHAPE_H
 
-#include "hb-common.h"
 #include "hb-shape.h"
 
 
