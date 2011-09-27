5857720cd35078d1c3906c8b2db3190b5166b66f
Behdad Esfahbod <behdad@behdad.org>
2011-09-27 12:36:26 -0400

    [util] s/%d/%u/ when printing glyph ids and clusters

--- util/options.cc
+++ util/options.cc
@@ -650,12 +650,12 @@ format_options_t::serialize (hb_buffer_t *buffer,
       if (!FT_Get_Glyph_Name (ft_face, info->codepoint, glyph_name, sizeof (glyph_name)))
 	g_string_append_printf (gs, "%s", glyph_name);
       else
-	g_string_append_printf (gs, "gid%d", info->codepoint);
+	g_string_append_printf (gs, "gid%u", info->codepoint);
     } else
-      g_string_append_printf (gs, "%d", info->codepoint);
+      g_string_append_printf (gs, "%u", info->codepoint);
 
     if (show_clusters)
-      g_string_append_printf (gs, "=%d", info->cluster);
+      g_string_append_printf (gs, "=%u", info->cluster);
 
     if (show_positions && (pos->x_offset || pos->y_offset)) {
       g_string_append_c (gs, '@');
