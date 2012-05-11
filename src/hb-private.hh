5ccfe8e2154ad0b58dabcc236bbe9478c17b02ab
Behdad Esfahbod <behdad@behdad.org>
2012-05-11 02:19:41 +0200

    /Minor/

--- src/hb-private.hh
+++ src/hb-private.hh
@@ -519,7 +519,7 @@ _hb_debug_msg_va (const char *what,
   fprintf (stderr, "%-10s", what ? what : "");
 
   if (obj)
-    fprintf (stderr, "(%0*x) ", (unsigned int) (2 * sizeof (void *)), (unsigned long) obj);
+    fprintf (stderr, "(%0*lx) ", (unsigned int) (2 * sizeof (void *)), (unsigned long) obj);
   else
     fprintf (stderr, " %*s  ", (unsigned int) (2 * sizeof (void *)), "");
 
