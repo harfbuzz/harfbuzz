harfbuzz/harfbuzz

commit a3313e54008167e415b72c780ca7b9cda958d07e

[GPOS] Fix MarkMarkPos applied to results of MultipleSubst
This was broken as a result of 7b84c536c10ab90ed96a033d88e9ad232d46c5b8.
As Khaled reported, MarkMark positioning was broken with glyphs
resulting from a MultipleSubst.  Fixed.  Test with the ALLAH character
in Amiri.

src/hb-ot-layout-gpos-table.hh
@@ -1169,11 +1169,11 @@ struct MarkMarkPosFormat1
     unsigned int j = skippy_iter.idx;
 
     /* Two marks match only if they belong to the same base, or same component
-     * of the same ligature.  That is, the component numbers must match, and
-     * if those are non-zero, the ligid number should also match. */
-    if ((get_lig_comp (c->buffer->info[j]) != get_lig_comp (c->buffer->cur())) ||
-	(get_lig_comp (c->buffer->info[j]) > 0 &&
-	 get_lig_id (c->buffer->info[j]) != get_lig_id (c->buffer->cur())))
+     * of the same ligature.  That is, the lig_id numbers must match, and
+     * if those are non-zero, the lig_comp number should also match. */
+    if ((get_lig_id (c->buffer->info[j]) != get_lig_id (c->buffer->cur())) ||
+	(get_lig_id (c->buffer->info[j]) > 0 &&
+	 get_lig_comp (c->buffer->info[j]) != get_lig_comp (c->buffer->cur())))
       return TRACE_RETURN (false);
 
     unsigned int mark2_index = (this+mark2Coverage) (c->buffer->info[j].codepoint);

src/hb-ot-layout-private.hh
@@ -68,8 +68,27 @@ _hb_ot_layout_skip_mark (hb_face_t    *face,
  * GSUB/GPOS
  */
 
-/* unique ligature id */
-/* component number in the ligature (0 = base) */
+/* lig_id / lig_comp
+ *
+ * When a ligature is formed:
+ *
+ *   - The ligature glyph and any marks in between all get a unique lig_id,
+ *   - The ligature glyph will get lig_comp = 0
+ *   - The marks get lig_comp > 0, reflecting which component of the ligature
+ *     they were applied to.
+ *   - This is used in GPOS to attach marks to the right component of a ligature
+ *     in MarkLigPos.
+ *
+ * When a multiple-substitution is done:
+ *
+ *   - All resulting glyphs will have lig_id = 0,
+ *   - The resulting glyphs will have lig_comp = 0, 1, 2, ... respectively.
+ *   - This is used in GPOS to attack marks to the first component of a
+ *     multiple substitution in MarkBasePos.
+ *
+ * The numbers are also used in GPOS to do mark-to-mark positioning only
+ * to marks that belong to the same component of a ligature in MarkMarPos.
+ */
 static inline void
 set_lig_props (hb_glyph_info_t &info, unsigned int lig_id, unsigned int lig_comp)
 {

commit 5d874d566fe5d2cc4cfaf02c79b663d8a626ca1e

[GPOS] Fix mark-to-mark positioning when one of the marks is a ligature
This commit: a3313e54008167e415b72c780ca7b9cda958d07e broke MarkMarkPos
when one of the marks itself is a ligature.  That regressed 26 Tibetan
tests (up from zero!).  Fix that.  Tibetan back to zero.

src/hb-ot-layout-gpos-table.hh
@@ -1263,14 +1263,27 @@ struct MarkMarkPosFormat1
 
     unsigned int j = skippy_iter.idx;
 
-    /* Two marks match only if they belong to the same base, or same component
-     * of the same ligature.  That is, the lig_id numbers must match, and
-     * if those are non-zero, the lig_comp number should also match. */
-    if ((get_lig_id (c->buffer->info[j]) != get_lig_id (c->buffer->cur())) ||
-	(get_lig_id (c->buffer->info[j]) > 0 &&
-	 get_lig_comp (c->buffer->info[j]) != get_lig_comp (c->buffer->cur())))
-      return TRACE_RETURN (false);
+    unsigned int id1 = get_lig_id (c->buffer->cur());
+    unsigned int id2 = get_lig_id (c->buffer->info[j]);
+    unsigned int comp1 = get_lig_comp (c->buffer->cur());
+    unsigned int comp2 = get_lig_comp (c->buffer->info[j]);
 
+    if (likely (id1 == id2)) {
+      if (id1 == 0) /* Marks belonging to the same base. */
+	goto good;
+      else if (comp1 == comp2) /* Marks belonging to the same ligature component. */
+        goto good;
+    } else {
+      /* If ligature ids don't match, it may be the case that one of the marks
+       * itself is a ligature.  In which case match. */
+      if ((id1 > 0 && !comp1) || (id2 > 0 && !comp2))
+	goto good;
+    }
+
+    /* Didn't match. */
+    return TRACE_RETURN (false);
+
+    good:
     unsigned int mark2_index = (this+mark2Coverage) (c->buffer->info[j].codepoint);
     if (mark2_index == NOT_COVERED) return TRACE_RETURN (false);
 

src/hb-ot-layout-private.hh
@@ -73,7 +73,8 @@ _hb_ot_layout_skip_mark (hb_face_t    *face,
  *
  * When a ligature is formed:
  *
- *   - The ligature glyph and any marks in between all get a unique lig_id,
+ *   - The ligature glyph and any marks in between all the same newly allocated
+ *     lig_id,
  *   - The ligature glyph will get lig_comp = 0
  *   - The marks get lig_comp > 0, reflecting which component of the ligature
  *     they were applied to.
@@ -84,7 +85,7 @@ _hb_ot_layout_skip_mark (hb_face_t    *face,
  *
  *   - All resulting glyphs will have lig_id = 0,
  *   - The resulting glyphs will have lig_comp = 0, 1, 2, ... respectively.
- *   - This is used in GPOS to attack marks to the first component of a
+ *   - This is used in GPOS to attach marks to the first component of a
  *     multiple substitution in MarkBasePos.
  *
  * The numbers are also used in GPOS to do mark-to-mark positioning only

test/shaping/texts/in-tree/shaper-indic/MANIFEST
@@ -1,2 +1,3 @@
 indic
+south-asian
 south-east-asian

test/shaping/texts/in-tree/shaper-indic/indic/script-sinhala/misc/MANIFEST
@@ -1,2 +1,3 @@
+extensive.txt
 misc.txt
 reph.txt

test/shaping/texts/in-tree/shaper-indic/south-asian/MANIFEST
@@ -1,0 +1,1 @@
+script-tibetan

test/shaping/texts/in-tree/shaper-indic/south-asian/script-tibetan/MANIFEST
@@ -1,0 +1,1 @@
+misc

test/shaping/texts/in-tree/shaper-indic/south-asian/script-tibetan/misc/MANIFEST
@@ -1,0 +1,1 @@
+misc.txt