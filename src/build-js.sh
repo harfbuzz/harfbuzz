7299933c720c1696bd041272d0f0cdb91fcd9ca3
Lorant Pinter <lorant.pinter@prezi.com>
2012-10-03 11:34:10 +0200

    Fixed build with new UCDN

--- src/build-js.sh
+++ src/build-js.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 make
-emcc .libs/libharfbuzz.dylib -o harfbuzz-unoptimized.js
+emcc .libs/libharfbuzz.dylib hb-ucdn/.libs/ucdn.o -o harfbuzz-unoptimized.js
 #sed -e '/GENERATED_CODE/r main-unoptimized.js' main-run.template.js > main-run-unoptimized.js
 #node main-run.js OpenBaskerville-0.0.75.otf
