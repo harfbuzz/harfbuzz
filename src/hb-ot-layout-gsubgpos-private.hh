harfbuzz/harfbuzz

commit 4c4e8f0e754b79ac6190d21878eaaf0b790c7579

[OTLayout] Reuse apply context for recursion

src/hb-ot-layout-gsubgpos-private.hh
@@ -233,10 +233,10 @@ struct hb_apply_context_t
     if (unlikely (nesting_level_left == 0 || !recurse_func))
       return default_return_value ();
 
-    /* TODO Reuse context. */
-    hb_apply_context_t new_c (*this);
-    new_c.nesting_level_left--;
-    return recurse_func (&new_c, lookup_index);
+    nesting_level_left--;
+    recurse_func (this, lookup_index);
+    nesting_level_left++;
+    return default_return_value ();
   }
 
   hb_font_t *font;

src/hb-ot-layout-gpos-table.hh
@@ -1537,8 +1537,13 @@ inline bool PosLookup::apply_recurse_func (hb_apply_context_t *c, unsigned int l
 {
   const GPOS &gpos = *(hb_ot_layout_from_face (c->face)->gpos);
   const PosLookup &l = gpos.get_lookup (lookup_index);
+  unsigned int saved_lookup_props = c->lookup_props;
+  unsigned int saved_property = c->property;
   c->set_lookup (l);
-  return l.apply_once (c);
+  bool ret = l.apply_once (c);
+  c->lookup_props = saved_lookup_props;
+  c->property = saved_property;
+  return ret;
 }
 
 

src/hb-ot-layout-gsub-table.hh
@@ -1402,8 +1402,13 @@ inline bool SubstLookup::apply_recurse_func (hb_apply_context_t *c, unsigned int
 {
   const GSUB &gsub = *(hb_ot_layout_from_face (c->face)->gsub);
   const SubstLookup &l = gsub.get_lookup (lookup_index);
+  unsigned int saved_lookup_props = c->lookup_props;
+  unsigned int saved_property = c->property;
   c->set_lookup (l);
-  return l.apply_once (c);
+  bool ret = l.apply_once (c);
+  c->lookup_props = saved_lookup_props;
+  c->property = saved_property;
+  return ret;
 }
 
 

commit f18ff5a84d9fe859c40a7a0c9a207cb40df0e84a

[OTLayout] Return correct value from recursion
Commit 4c4e8f0e754b79ac6190d21878eaaf0b790c7579 broke contextual lookups
by making the recurse() function always return false.

Reported by Khaled.  Test case: لا in Amiri.

src/hb-ot-layout-gsubgpos-private.hh
@@ -71,7 +71,7 @@ struct hb_closure_context_t
     nesting_level_left--;
     recurse_func (this, lookup_index);
     nesting_level_left++;
-    return default_return_value ();
+    return VOID;
   }
 
   hb_face_t *face;
@@ -112,7 +112,6 @@ struct hb_would_apply_context_t
   inline return_t process (const T &obj) { return obj.would_apply (this); }
   static return_t default_return_value (void) { return false; }
   bool stop_sublookup_iteration (const return_t r) const { return r; }
-  return_t recurse (unsigned int lookup_index) { return true; }
 
   hb_face_t *face;
   const hb_codepoint_t *glyphs;
@@ -162,7 +161,7 @@ struct hb_collect_glyphs_context_t
     hb_collect_glyphs_context_t new_c (this->face, NULL, NULL, NULL, &output, nesting_level_left);
     recurse_func (&new_c, lookup_index);
     nesting_level_left++;
-    return default_return_value ();
+    return VOID;
   }
 
   hb_face_t *face;
@@ -236,9 +235,9 @@ struct hb_apply_context_t
       return default_return_value ();
 
     nesting_level_left--;
-    recurse_func (this, lookup_index);
+    bool ret = recurse_func (this, lookup_index);
     nesting_level_left++;
-    return default_return_value ();
+    return ret;
   }
 
   hb_font_t *font;