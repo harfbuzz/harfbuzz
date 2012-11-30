8465a05a89742295d4b009a9b4442eb0a899fd80
Behdad Esfahbod <behdad@behdad.org>
2012-11-30 08:46:43 +0200

    Fix hb_buffer_guess_segment_properties() for empty buffer
    Was causing assertion failure in shape_plan().

--- src/hb-buffer.cc
+++ src/hb-buffer.cc
@@ -493,8 +493,8 @@ hb_buffer_t::merge_out_clusters (unsigned int start,
 void
 hb_buffer_t::guess_segment_properties (void)
 {
-  if (unlikely (!len)) return;
-  assert (content_type == HB_BUFFER_CONTENT_TYPE_UNICODE);
+  assert (content_type == HB_BUFFER_CONTENT_TYPE_UNICODE ||
+	  (!len && content_type == HB_BUFFER_CONTENT_TYPE_INVALID));
 
   /* If script is set to INVALID, guess from buffer contents */
   if (props.script == HB_SCRIPT_INVALID) {
