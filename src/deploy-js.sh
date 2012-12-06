5e0df91df37598a58d11e17a4aea8cec590f0b7c
Lorant Pinter <lorant.pinter@prezi.com>
2012-12-06 12:13:54 +0100

    Fixed typo

--- src/deploy-js.sh
+++ src/deploy-js.sh
@@ -2,5 +2,5 @@
 
 VERSION=`git describe`
 
-mvn org.apache.maven.plugins:maven-deploy-plugin:2.7:deploy-file -DrepositoryId=nexus -Durl=http://intra.prezi.com:8081/nexus/content/repositories/releases/ -DartifactId=harfbuzz-js -DgroupId=org.harfbuzz -Dversion=$VERSION -Dfile=harfbuzz.js -Dtype=js -Dfiles=harfbuzz-untyped.js,harfbuzz-unoptimized.js,harfbuzz-untyped-unoptimized.js -Dtypes=ks,js,js -DuniqueVersion=false -Dclassifiers=untyped,debug,untyped-debug
+mvn org.apache.maven.plugins:maven-deploy-plugin:2.7:deploy-file -DrepositoryId=nexus -Durl=http://intra.prezi.com:8081/nexus/content/repositories/releases/ -DartifactId=harfbuzz-js -DgroupId=org.harfbuzz -Dversion=$VERSION -Dfile=harfbuzz.js -Dtype=js -Dfiles=harfbuzz-untyped.js,harfbuzz-unoptimized.js,harfbuzz-untyped-unoptimized.js -Dtypes=js,js,js -DuniqueVersion=false -Dclassifiers=untyped,debug,untyped-debug
 
