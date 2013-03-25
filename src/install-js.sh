adc5a3bf1f17d3a1a3fdb8a9774ff205ac479f4f
Lorant Pinter <lorant.pinter@prezi.com>
2013-03-25 19:34:11 +0100

    Removed build scripts (use Gradle instead)

--- src/install-js.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION=`git describe`
-
-mvn install:install-file -DartifactId=harfbuzz-js -DgroupId=org.harfbuzz -Dversion=$VERSION -Dfile=harfbuzz.js -Dpackaging=js -Dtype=js 
-mvn install:install-file -DartifactId=harfbuzz-js -DgroupId=org.harfbuzz -Dversion=$VERSION -Dfile=harfbuzz-untyped.js -Dpackaging=js -Dtype=js -Dclassifier=untyped 
-mvn install:install-file -DartifactId=harfbuzz-js -DgroupId=org.harfbuzz -Dversion=$VERSION -Dfile=harfbuzz-unoptimized.js -Dpackaging=js -Dtype=js -Dclassifier=debug 
-mvn install:install-file -DartifactId=harfbuzz-js -DgroupId=org.harfbuzz -Dversion=$VERSION -Dfile=harfbuzz-untyped-unoptimized.js -Dpackaging=js -Dtype=js -Dclassifier=untyped-debug 
-
