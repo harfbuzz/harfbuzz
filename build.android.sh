ccd09e0b6f057f6fbfb0391cdddb2f1d5b4caa05
Peter Nemeth <peter.nemeth@prezi.com>
2013-04-27 16:03:14 +0200

    Add ARCH option when cross-compiling to android.

--- build.android.sh
+++ build.android.sh
@@ -1,7 +1,23 @@
-#!/bin/bash
+#!/bin/bash -e
 PWD="$(pwd)"
 ANDROID_PLATFORM=android-14
 DESTDIR="$PWD/build"
+ARCH=arm
+
+if [ ! -z "$1" ]; then
+    ARCH=$1
+    if [ "$ARCH" != "arm" -a "$ARCH" != "x86" ]; then
+        echo "Unknown arhitecture: $ARCH"
+        exit 1
+    fi
+fi
+
+case $ARCH in
+    arm) HOST=arm-linux-androideabi
+        ;;
+    x86) HOST=x86
+        ;;
+esac
 
 if [ -z "$ANDROID_NDK" ]; then
     echo "ANDROID_NDK not set (e.g. ~/android/android-ndk-r8d)"
@@ -17,13 +33,14 @@ fi
 
 rm -rf $DESTDIR
 TMP="$DESTDIR/.tmp-build-hb"
-TOOLCHAIN_DIR="$TMP/ndk-standalone"
+TOOLCHAIN_DIR="$TMP/ndk-standalone-$ARCH"
 "$ANDROID_NDK/build/tools/make-standalone-toolchain.sh" \
+        --arch=$ARCH \
         --platform=$ANDROID_PLATFORM \
         --install-dir=$TOOLCHAIN_DIR
 PATH=$TOOLCHAIN_DIR/bin:$PATH
 
-CFLAGS="-std=gnu99" ./configure --host=arm-linux-androideabi --prefix=$DESTDIR --disable-shared --enable-static && make clean >/dev/null && make && make install
+CFLAGS="-std=gnu99" ./configure --host=$HOST --prefix=$DESTDIR --disable-shared --enable-static && make clean >/dev/null && make && make install
 
 cp -f src/*-private.hh "$DESTDIR"
 
