10f964623f003c70f6bdd33423420abda3820ce0
Behdad Esfahbod <behdad@behdad.org>
2013-08-09 09:40:01 -0400

    Round when scaling values
    Doesn't matter for most users since they should be working in a
    fixed sub-pixel scale anyway (ie. 22.10, 26.6, 16.16, etc).

--- src/hb-font-private.hh
+++ src/hb-font-private.hh
@@ -461,7 +461,11 @@ struct hb_font_t {
   }
 
   private:
-  inline hb_position_t em_scale (int16_t v, int scale) { return v * (int64_t) scale / face->get_upem (); }
+  inline hb_position_t em_scale (int16_t v, int scale)
+  {
+    unsigned int upem = face->get_upem ();
+    return (v * (int64_t) scale + upem / 2) / upem;
+  }
 };
 
 #define HB_SHAPER_DATA_CREATE_FUNC_EXTRA_ARGS
