4eb5b08da7d029162c6c58d82a75b87d688035b2
Sylvestre Ledru <sylvestre@debian.org>
2014-01-23 00:21:15 +0100

    remove clang warning 'private field 'xxx' is not used

--- src/hb-open-type-private.hh
+++ src/hb-open-type-private.hh
@@ -592,7 +592,7 @@ struct LONGDATETIME
     TRACE_SANITIZE (this);
     return TRACE_RETURN (likely (c->check_struct (this)));
   }
-  private:
+  protected:
   LONG major;
   ULONG minor;
   public:
