ee64c19f4a2f3a02cfe87e4a7ac003267fae4f45
Behdad Esfahbod <behdad@behdad.org>
2014-03-19 11:52:40 -0700

    Yet another try to make build without gtk-doc succeed

--- autogen.sh
+++ autogen.sh
@@ -24,6 +24,7 @@ if which gtkdocize ; then
 	gtkdocize --copy || exit 1
 else
 	echo "*** No gtkdocize found, skipping documentation ***"
+	> gtk-doc.make
 fi
 
 echo -n "checking for autoreconf... "
