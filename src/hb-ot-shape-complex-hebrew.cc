9e834e29e0b657f0555df1ab9cea79ff7abcf08d
Behdad Esfahbod <behdad@behdad.org>
2014-07-26 20:34:01 -0400

    [hebrew] Zero mark advance by GDEF late
    Seems to be what Uniscribe does.
    
    At this point I think it's work checking our default...
    
    Fixes Bug 76767 - Zeroing of advance of 2nd component of multiple
    substitution with SBL Hebrew
    https://bugs.freedesktop.org/show_bug.cgi?id=76767
    
    Micro-test added.

--- src/hb-ot-shape-complex-hebrew.cc
+++ src/hb-ot-shape-complex-hebrew.cc
@@ -167,6 +167,6 @@ const hb_ot_complex_shaper_t _hb_ot_complex_shaper_hebrew =
   NULL, /* decompose */
   compose_hebrew,
   NULL, /* setup_masks */
-  HB_OT_SHAPE_ZERO_WIDTH_MARKS_DEFAULT,
+  HB_OT_SHAPE_ZERO_WIDTH_MARKS_BY_GDEF_LATE,
   true, /* fallback_position */
 };
