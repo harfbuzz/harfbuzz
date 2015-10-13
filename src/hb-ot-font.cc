ed13e2ce509408f849255be1d3cc3dbd7dbb3ba2
Behdad Esfahbod <behdad@behdad.org>
2015-10-13 10:32:56 -0300

    [ot-font] Fix leak
    https://github.com/behdad/harfbuzz/issues/139#issuecomment-147616887

--- src/hb-ot-font.cc
+++ src/hb-ot-font.cc
@@ -261,6 +261,7 @@ _hb_ot_font_destroy (hb_ot_font_t *ot_font)
   ot_font->cmap.fini ();
   ot_font->h_metrics.fini ();
   ot_font->v_metrics.fini ();
+  ot_font->glyf.fini ();
 
   free (ot_font);
 }
