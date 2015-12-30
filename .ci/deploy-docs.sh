266bfa3772e75fedcd93db22bf8672a13ad7a7d9
Behdad Esfahbod <behdad@behdad.org>
2015-12-30 16:25:48 +0100

    [docs] Minor

--- .ci/deploy-docs.sh
+++ .ci/deploy-docs.sh
@@ -21,7 +21,7 @@ git init
 git config user.name "Travis CI"
 git config user.email "travis@harfbuzz.org"
 set +x
-echo "git remote add upstream \"https://$$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git\""
+echo "git remote add upstream \"https://\$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git\""
 git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 set -x
 git fetch upstream
