f3a051149b3b565b5ad4e489bf730c5203c3549e
Behdad Esfahbod <behdad@behdad.org>
2015-12-30 15:45:43 +0100

    [docs] Shuffling code around in deploy-docs.sh
    Temporarily makes it rebuild docs on every change again.

--- .ci/deploy-docs.sh
+++ .ci/deploy-docs.sh
@@ -1,27 +1,35 @@
-TAG="$(git describe --exact-match --match "[0-9]*" HEAD 2>/dev/null)"
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_OS_NAME" == "linux" -a "$CC" == "gcc" -a "$TRAVIS_SECURE_ENV_VARS" == "true" -a "$TAG" != "" ]
-then
-	DOCSDIR=build-docs
-	REVISION=$(git rev-parse --short HEAD)
-
-	rm -rf $DOCSDIR || exit
-	mkdir $DOCSDIR
-	cd $DOCSDIR
-
-	cp ../docs/html/* .
-
-	git init
-	git config user.name "Travis CI"
-	git config user.email "travis@harfbuzz.org"
-	git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
-	git fetch upstream
-	git reset upstream/gh-pages
-
-	touch .
-	git add -A .
-	git commit -m "Rebuild docs for $REVISION"
-	git push -q upstream HEAD:gh-pages
+if [ "$TRAVIS_OS_NAME" != "linux" -o "$CC" != "gcc" -o "$TRAVIS_SECURE_ENV_VARS" != "true" ]; then
+	exit
 fi
+
+BRANCH="$(TRAVIS_BRANCH)"
+TAG="$(git describe --exact-match --match "[0-9]*" HEAD 2>/dev/null)"
+
+if [ "x$TAG" == x ]; then
+	REVISION=$(git rev-parse --short HEAD)
+else
+	REVISION=$TAG
+fi
+
+DOCSDIR=build-docs
+
+rm -rf $DOCSDIR || exit
+mkdir $DOCSDIR
+cd $DOCSDIR
+
+cp ../docs/html/* .
+
+git init
+git config user.name "Travis CI"
+git config user.email "travis@harfbuzz.org"
+git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+git add -A .
+git commit -m "Rebuild docs for $REVISION"
+git push -q upstream HEAD:gh-pages
