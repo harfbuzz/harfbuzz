a0f1b44b6c0e3b1e6a1ba63dde2be22e0425353f
Khaled Hosny <khaledhosny@eglug.org>
2016-08-16 03:09:04 +0200

    [coretext] Blind fix for build on MacOS 10.9

--- src/hb-coretext.cc
+++ src/hb-coretext.cc
@@ -150,7 +150,7 @@ create_ct_font (CGFontRef cg_font, CGFloat font_size)
    * bug indicate that the cascade list reconfiguration occasionally causes
    * crashes in CoreText on OS X 10.9, thus let's skip this step on older
    * operating system versions. */
-  if (&CTGetCoreTextVersion != NULL && CTGetCoreTextVersion() < kCTVersionNumber10_10)
+  if (&CTGetCoreTextVersion != NULL && CTGetCoreTextVersion() <= kCTVersionNumber10_9)
     return ct_font;
 
   CFURLRef original_url = (CFURLRef)CTFontCopyAttribute(ct_font, kCTFontURLAttribute);
