a5629a2ed9c2e69f2b4ebb74d85fbe0b5ba2be67
Behdad Esfahbod <behdad@behdad.org>
2016-09-27 11:30:44 +0200

    [MATH] Simplify test

--- test/api/test-ot-math.c
+++ test/api/test-ot-math.c
@@ -319,10 +319,9 @@ get_glyph_assembly_italics_correction (hb_font_t *font,
 				       hb_bool_t horizontal)
 {
   hb_position_t corr;
-  unsigned int count = 0;
   hb_ot_math_get_glyph_assembly (font, glyph,
 				 horizontal ? HB_DIRECTION_LTR : HB_DIRECTION_TTB,
-				 0, &count, NULL,
+				 0, NULL, NULL,
 				 &corr);
   return corr;
 }
