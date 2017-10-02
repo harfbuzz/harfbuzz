819cc360499a5f136325b5a1a18098ebc2aee081
Behdad Esfahbod <behdad@behdad.org>
2017-10-02 09:03:16 -0400

    [indic] Move manual code out of generated table

--- src/hb-ot-shape-complex-indic.cc
+++ src/hb-ot-shape-complex-indic.cc
@@ -209,6 +209,11 @@ set_indic_properties (hb_glyph_info_t &info)
     pos = POS_ABOVE_C;
   }
 
+  /* According to ScriptExtensions.txt, these Grantha marks may also be used in Tamil,
+   * so the Indic shaper needs to know their categories. */
+  else if (unlikely (u == 0x11303u)) cat = OT_SM;
+  else if (unlikely (u == 0x1133cu)) cat = OT_N;
+
   else if (unlikely (u == 0x0980u)) cat = OT_PLACEHOLDER; /* https://github.com/behdad/harfbuzz/issues/538 */
   else if (unlikely (u == 0x17C6u)) cat = OT_N; /* Khmer Bindu doesn't like to be repositioned. */
   else if (unlikely (hb_in_range<hb_codepoint_t> (u, 0x2010u, 0x2011u)))
