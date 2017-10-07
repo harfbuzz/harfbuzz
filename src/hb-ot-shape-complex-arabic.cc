5f50f8837f918ca1d176355d1538e0e6c9703f41
Behdad Esfahbod <behdad@behdad.org>
2017-10-07 13:17:33 +0200

    [arabic] Fix cluster merging

--- src/hb-ot-shape-complex-arabic.cc
+++ src/hb-ot-shape-complex-arabic.cc
@@ -676,7 +676,7 @@ reorder_marks_arabic (const hb_ot_shape_plan_t *plan,
     DEBUG_MSG (ARABIC, buffer, "Shifting %d's: %d %d\n", cc, i, j);
     hb_glyph_info_t temp[HB_OT_SHAPE_COMPLEX_MAX_COMBINING_MARKS];
     assert (j - i <= ARRAY_LENGTH (temp));
-    buffer->merge_out_clusters (start, j);
+    buffer->merge_clusters (start, j);
     memmove (temp, &info[i], (j - i) * sizeof (hb_glyph_info_t));
     memmove (&info[start + j - i], &info[start], (i - start) * sizeof (hb_glyph_info_t));
     memmove (&info[start], temp, (j - i) * sizeof (hb_glyph_info_t));
