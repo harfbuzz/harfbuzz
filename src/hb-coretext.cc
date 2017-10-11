717fcb51dd051ca27d8537e5cf3a32d0447f78b5
Behdad Esfahbod <behdad@behdad.org>
2017-10-11 15:38:21 +0200

    [coretext] Minor

--- src/hb-coretext.cc
+++ src/hb-coretext.cc
@@ -37,7 +37,7 @@
 #endif
 
 /* https://developer.apple.com/documentation/coretext/1508745-ctfontcreatewithgraphicsfont */
-#define HB_CORETEXT_FONT_SIZE 12.0
+#define HB_CORETEXT_DEFAULT_FONT_SIZE 12.0
 
 static void
 release_table_data (void *user_data)
@@ -74,7 +74,7 @@ hb_coretext_face_create (CGFontRef cg_font)
 HB_SHAPER_DATA_ENSURE_DEFINE(coretext, face)
 HB_SHAPER_DATA_ENSURE_DEFINE_WITH_CONDITION(coretext, font,
                                             abs (CTFontGetSize((CTFontRef) data) -
-                                                 (font->ptem <= 0 ? HB_CORETEXT_FONT_SIZE : font->ptem)) < 1)
+                                                 (font->ptem <= 0 ? HB_CORETEXT_DEFAULT_FONT_SIZE : font->ptem)) < 1)
 
 /*
  * shaper face data
@@ -247,7 +247,7 @@ _hb_coretext_shaper_font_data_create (hb_font_t *font)
   if (unlikely (!hb_coretext_shaper_face_data_ensure (face))) return NULL;
   CGFontRef cg_font = (CGFontRef) HB_SHAPER_DATA_GET (face);
 
-  float ptem = font->ptem <= 0 ? HB_CORETEXT_FONT_SIZE : font->ptem;
+  float ptem = font->ptem <= 0 ? HB_CORETEXT_DEFAULT_FONT_SIZE : font->ptem;
 
   CTFontRef ct_font = create_ct_font (cg_font, ptem);
 
