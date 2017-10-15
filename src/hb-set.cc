1d3971200be5b1c949d3eca185654e48584a0868
Behdad Esfahbod <behdad@behdad.org>
2017-10-15 16:15:24 +0200

    Deprecate hb_set_invert()

--- src/hb-set.cc
+++ src/hb-set.cc
@@ -376,11 +376,12 @@ hb_set_symmetric_difference (hb_set_t       *set,
  * 
  *
  * Since: 0.9.10
+ *
+ * Deprecated: 1.6.1
  **/
 void
 hb_set_invert (hb_set_t *set)
 {
-  set->invert ();
 }
 
 /**
