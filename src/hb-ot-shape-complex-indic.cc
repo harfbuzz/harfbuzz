0f9d118cf9e3a52ff5a78dee1c47a521033acdf9
David Corbett <corbett.dav@husky.neu.edu>
2017-11-20 13:19:34 -0500

    Categorize U+0C80 as Consonant_Placeholder

--- src/hb-ot-shape-complex-indic.cc
+++ src/hb-ot-shape-complex-indic.cc
@@ -223,6 +223,7 @@ set_indic_properties (hb_glyph_info_t &info)
   else if (unlikely (u == 0x0AFBu)) cat = OT_N; /* https://github.com/harfbuzz/harfbuzz/issues/552 */
 
   else if (unlikely (u == 0x0980u)) cat = OT_PLACEHOLDER; /* https://github.com/harfbuzz/harfbuzz/issues/538 */
+  else if (unlikely (u == 0x0C80u)) cat = OT_PLACEHOLDER; /* https://github.com/harfbuzz/harfbuzz/pull/623 */
   else if (unlikely (u == 0x17C6u)) cat = OT_N; /* Khmer Bindu doesn't like to be repositioned. */
   else if (unlikely (hb_in_range<hb_codepoint_t> (u, 0x2010u, 0x2011u)))
 				    cat = OT_PLACEHOLDER;
