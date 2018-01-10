0b22da954142ad7a1e949a56448cee4e836fff75
Khaled Hosny <khaledhosny@eglug.org>
2018-01-10 07:12:07 +0200

    Improve HB_BUFFER_SERIALIZE_FLAG_NO_ADVANCES docs
    Add the note about absolute glyph positions from the commit message.

--- src/hb-buffer.h
+++ src/hb-buffer.h
@@ -422,7 +422,8 @@ hb_buffer_normalize_glyphs (hb_buffer_t *buffer);
  * @HB_BUFFER_SERIALIZE_FLAG_NO_GLYPH_NAMES: do no serialize glyph name.
  * @HB_BUFFER_SERIALIZE_FLAG_GLYPH_EXTENTS: serialize glyph extents.
  * @HB_BUFFER_SERIALIZE_FLAG_GLYPH_FLAGS: serialize glyph flags. Since: 1.5.0
- * @HB_BUFFER_SERIALIZE_FLAG_NO_ADVANCES: do not serialize glyph advances. Since: 1.8.0
+ * @HB_BUFFER_SERIALIZE_FLAG_NO_ADVANCES: do not serialize glyph advances,
+ *  glyph offsets will reflect absolute glyph positions. Since: 1.8.0
  *
  * Flags that control what glyph information are serialized in hb_buffer_serialize_glyphs().
  *
