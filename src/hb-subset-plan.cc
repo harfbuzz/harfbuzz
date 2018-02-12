afb1da3a1891b7c0fdd047bcb7b3bde86e830444
Rod Sheeter <rsheeter@google.com>
2018-02-12 14:37:47 -0800

    auto-completed the wrong gids_to_retain

--- src/hb-subset-plan.cc
+++ src/hb-subset-plan.cc
@@ -62,8 +62,8 @@ hb_subset_plan_new_gid_for_old_id (hb_subset_plan_t *plan,
                                    hb_codepoint_t *new_gid)
 {
   // the index in old_gids is the new gid; only up to codepoints.len are valid
-  for (unsigned int i = 0; i < plan->gids_to_retain_sorted.len; i++) {
-    if (plan->gids_to_retain_sorted[i] == old_gid) {
+  for (unsigned int i = 0; i < plan->gids_to_retain.len; i++) {
+    if (plan->gids_to_retain[i] == old_gid) {
       // +1: assign new gids from 1..N; 0 is special
       *new_gid = i + 1;
       return true;
