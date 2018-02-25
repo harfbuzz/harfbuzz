ae14dd0fb0a176f6f0143690907b51626999d248
Ebrahim Byagowi <ebrahim@gnu.org>
2018-02-26 00:31:09 +0330

    [aat] Implement ankr

--- src/hb-ot-layout-private.hh
+++ src/hb-ot-layout-private.hh
@@ -131,8 +131,9 @@ namespace OT {
 }
 
 namespace AAT {
-  struct morx;
+  struct ankr;
   struct kerx;
+  struct morx;
   struct trak;
 }
 
@@ -171,8 +172,9 @@ struct hb_ot_layout_t
   OT::hb_lazy_table_loader_t<struct OT::MATH> math;
   OT::hb_lazy_table_loader_t<struct OT::fvar> fvar;
   OT::hb_lazy_table_loader_t<struct OT::avar> avar;
-  OT::hb_lazy_table_loader_t<struct AAT::morx> morx;
+  OT::hb_lazy_table_loader_t<struct AAT::ankr> ankr;
   OT::hb_lazy_table_loader_t<struct AAT::kerx> kerx;
+  OT::hb_lazy_table_loader_t<struct AAT::morx> morx;
   OT::hb_lazy_table_loader_t<struct AAT::trak> trak;
 
   unsigned int gsub_lookup_count;
