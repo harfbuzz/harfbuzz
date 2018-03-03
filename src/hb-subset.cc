64cf53d6197a0e6c56aef744ceff2f8a0ff85654
Garret Rieger <grieger@google.com>
2018-03-02 17:33:49 -0800

    [subset] Subset vmtx if present.

--- src/hb-subset.cc
+++ src/hb-subset.cc
@@ -252,6 +252,9 @@ _subset_table (hb_subset_plan_t *plan,
     case HB_OT_TAG_hmtx:
       result = _subset<const OT::hmtx> (plan);
       break;
+    case HB_OT_TAG_vmtx:
+      result = _subset<const OT::vmtx> (plan);
+      break;
     case HB_OT_TAG_maxp:
       result = _subset<const OT::maxp> (plan);
       break;
