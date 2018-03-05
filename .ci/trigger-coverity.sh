5895fe3d4d0441dacf8989740cf4150193397796
Ebrahim Byagowi <ebrahim@gnu.org>
2018-03-05 12:58:34 +0330

    [ci] minor

--- .ci/trigger-coverity.sh
+++ .ci/trigger-coverity.sh
@@ -3,10 +3,8 @@
 set -x
 set -o errexit -o nounset
 
-if test "x$TRAVIS_EVENT_TYPE" != x"cron"; then exit; fi
-
-BRANCH="$TRAVIS_BRANCH"
-if test "x$BRANCH" != xmaster; then exit; fi
+if test x"$TRAVIS_EVENT_TYPE" != x"cron"; then exit; fi
+if test x"$TRAVIS_BRANCH" != x"master"; then exit; fi
 
 git fetch --unshallow
 git remote add upstream "https://$GH_TOKEN@github.com/harfbuzz/harfbuzz.git"
