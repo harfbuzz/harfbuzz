2fd458d437e42f7d9ea7c7229e7a7652a34222f6
Garret Rieger <grieger@google.com>
2018-03-15 13:04:31 -0700

    [subset] Rename hb-fuzzer -> hb-shape-fuzzer.

--- test/fuzzing/run-shape-fuzzer-tests.py
+++ test/fuzzing/run-shape-fuzzer-tests.py
@@ -6,23 +6,23 @@ import sys, os, subprocess
 srcdir = os.environ.get ("srcdir", ".")
 EXEEXT = os.environ.get ("EXEEXT", "")
 top_builddir = os.environ.get ("top_builddir", ".")
-hb_fuzzer = os.path.join (top_builddir, "hb-fuzzer" + EXEEXT)
+hb_shape_fuzzer = os.path.join (top_builddir, "hb-shape-fuzzer" + EXEEXT)
 
-if not os.path.exists (hb_fuzzer):
+if not os.path.exists (hb_shape_fuzzer):
 	if len (sys.argv) == 1 or not os.path.exists (sys.argv[1]):
-		print ("""Failed to find hb-fuzzer binary automatically,
+		print ("""Failed to find hb-shape-fuzzer binary automatically,
 please provide it as the first argument to the tool""")
 		sys.exit (1)
 
-	hb_fuzzer = sys.argv[1]
+	hb_shape_fuzzer = sys.argv[1]
 
-print ('hb_fuzzer:', hb_fuzzer)
+print ('hb_shape_fuzzer:', hb_shape_fuzzer)
 fails = 0
 
 for line in open (os.path.join (srcdir, "..", "shaping", "data", "in-house", "tests", "fuzzed.tests")):
 	font = line.split (":")[0]
 
-	p = subprocess.Popen ([hb_fuzzer, os.path.join (srcdir, "..", "shaping", font)])
+	p = subprocess.Popen ([hb_shape_fuzzer, os.path.join (srcdir, "..", "shaping", font)])
 
 	if p.wait () != 0:
 		fails = fails + 1
