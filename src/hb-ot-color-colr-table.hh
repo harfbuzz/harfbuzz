a62554af89b8324c73c623e64f87ec822c757515
Ebrahim Byagowi <ebrahim@gnu.org>
2018-04-10 00:53:50 +0430

    [colr/cpal] Improvements and add a sample renderer (#927)

--- src/hb-ot-color-colr-table.hh
+++ src/hb-ot-color-colr-table.hh
@@ -64,6 +64,10 @@ struct BaseGlyphRecord
     return_trace (c->check_struct (this));
   }
 
+  inline int cmp (hb_codepoint_t g) const {
+    return g < glyphid ? -1 : g > glyphid ? 1 : 0;
+  }
+
   protected:
   GlyphID	glyphid;	/* Glyph ID of reference glyph */
   HBUINT16	firstLayerIdx;	/* Index to the layer record */
@@ -71,6 +75,13 @@ struct BaseGlyphRecord
   public:
   DEFINE_SIZE_STATIC (6);
 };
+
+static int compare_bgr (const void *pa, const void *pb)
+{
+  const hb_codepoint_t *a = (const hb_codepoint_t *) pa;
+  const BaseGlyphRecord *b = (const BaseGlyphRecord *) pb;
+  return b->cmp (*a);
+}
 
 struct COLR
 {
@@ -80,17 +91,41 @@ struct COLR
   {
     TRACE_SANITIZE (this);
     return_trace (c->check_struct (this) &&
-		  (this+baseGlyphs).sanitize (c, numBaseGlyphs) &&
-		  (this+layers).sanitize (c, numLayers));
+		  (this+baseGlyphsZ).sanitize (c, numBaseGlyphs) &&
+		  (this+layersZ).sanitize (c, numLayers));
+  }
+
+  inline bool get_base_glyph_record (hb_codepoint_t glyph_id,
+				     unsigned int *first_layer /* OUT */,
+				     unsigned int *num_layers /* OUT */) const
+  {
+    const BaseGlyphRecord* record;
+    record = (BaseGlyphRecord *) bsearch (&glyph_id, &(this+baseGlyphsZ), numBaseGlyphs,
+					  sizeof (BaseGlyphRecord), compare_bgr);
+    if (!record)
+      return false;
+
+    *first_layer = record->firstLayerIdx;
+    *num_layers = record->numLayers;
+    return true;
+  }
+
+  inline void get_layer_record (unsigned int record,
+				hb_codepoint_t *glyph_id /* OUT */,
+				unsigned int *palette_index /* OUT */) const
+  {
+    const LayerRecord &layer = (this+layersZ)[record];
+    *glyph_id = layer.glyphid;
+    *palette_index = layer.colorIdx;
   }
 
   protected:
   HBUINT16	version;	/* Table version number */
   HBUINT16	numBaseGlyphs;	/* Number of Base Glyph Records */
   LOffsetTo<UnsizedArrayOf<BaseGlyphRecord> >
-		baseGlyphs;	/* Offset to Base Glyph records. */
+		baseGlyphsZ;	/* Offset to Base Glyph records. */
   LOffsetTo<UnsizedArrayOf<LayerRecord> >
-		layers;		/* Offset to Layer Records */
+		layersZ;	/* Offset to Layer Records */
   HBUINT16	numLayers;	/* Number of Layer Records */
   public:
   DEFINE_SIZE_STATIC (14);
