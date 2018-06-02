fb07d1a3ce896f8ebf8b5d5fef51dee91b6ead11
Behdad Esfahbod <behdad@behdad.org>
2018-06-01 17:32:07 -0700

    Another attempt at making every compiler happy...
    Sigh.

--- src/hb-private.hh
+++ src/hb-private.hh
@@ -537,11 +537,11 @@ _hb_ceil_to_4 (unsigned int v)
  */
 
 #define HB_DISALLOW_ASSIGN(TypeName) \
-  void operator=(const TypeName&) = delete
+  void operator=(const TypeName&)
 
 #define HB_DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&) = delete; \
-  void operator=(const TypeName&) = delete
+  TypeName(const TypeName&); \
+  void operator=(const TypeName&)
 
 /*
  * Static pools
