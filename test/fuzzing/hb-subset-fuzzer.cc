37eab27be3b88079614f66e484c700bb2d40af10
Garret Rieger <grieger@google.com>
2018-06-07 14:39:03 -0700

    [subset] Add fuzzing of gsub closure to hb-subset-fuzzer.

--- test/fuzzing/hb-subset-fuzzer.cc
+++ test/fuzzing/hb-subset-fuzzer.cc
@@ -6,16 +6,17 @@
 
 #include "hb-subset.h"
 
-
 void trySubset (hb_face_t *face,
                 const hb_codepoint_t text[],
                 int text_length,
-                bool drop_hints)
+                bool drop_hints,
+                bool drop_ot_layout)
 {
   hb_subset_profile_t *profile = hb_subset_profile_create ();
 
   hb_subset_input_t *input = hb_subset_input_create_or_fail ();
-  *hb_subset_input_drop_hints(input) = drop_hints;
+  *hb_subset_input_drop_hints (input) = drop_hints;
+  *hb_subset_input_drop_ot_layout (input) = drop_ot_layout;
   hb_set_t *codepoints = hb_subset_input_unicode_set (input);
 
   for (int i = 0; i < text_length; i++)
@@ -30,6 +31,20 @@ void trySubset (hb_face_t *face,
   hb_subset_profile_destroy (profile);
 }
 
+void trySubset (hb_face_t *face,
+                const hb_codepoint_t text[],
+                int text_length)
+{
+  for (unsigned int drop_hints = 0; drop_hints < 2; drop_hints++)
+  {
+    for (unsigned int drop_ot_layout = 0; drop_ot_layout < 2; drop_ot_layout++)
+    {
+      trySubset (face, text, text_length,
+                 (bool) drop_hints, (bool) drop_ot_layout);
+    }
+  }
+}
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
   hb_blob_t *blob = hb_blob_create ((const char *)data, size,
@@ -42,8 +57,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
         '3', '@', '_', '%', '&', ')', '*', '$', '!'
       };
 
-  trySubset (face, text, sizeof (text) / sizeof (hb_codepoint_t), true);
-  trySubset (face, text, sizeof (text) / sizeof (hb_codepoint_t), false);
+  trySubset (face, text, sizeof (text) / sizeof (hb_codepoint_t));
 
   hb_codepoint_t text_from_data[16];
   if (size > sizeof(text_from_data)) {
@@ -51,8 +65,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
            data + size - sizeof(text_from_data),
            sizeof(text_from_data));
     unsigned int text_size = sizeof (text_from_data) / sizeof (hb_codepoint_t);
-    trySubset (face, text_from_data, text_size, true);
-    trySubset (face, text_from_data, text_size, false);
+    trySubset (face, text_from_data, text_size);
   }
 
   hb_face_destroy (face);
