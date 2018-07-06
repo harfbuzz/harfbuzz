27e2b0d7bb71f02ac5c0ee6116626323847242d4
David Corbett <corbett.dav@husky.neu.edu>
2018-07-06 15:47:03 -0400

    Do not enforce a native direction of LTR for Runic
    Fixes #481

--- src/hb-common.cc
+++ src/hb-common.cc
@@ -545,6 +545,7 @@ hb_script_get_horizontal_direction (hb_script_t script)
 
     /* https://github.com/harfbuzz/harfbuzz/issues/1000 */
     case HB_SCRIPT_OLD_ITALIC:
+    case HB_SCRIPT_RUNIC:
 
       return HB_DIRECTION_INVALID;
   }
