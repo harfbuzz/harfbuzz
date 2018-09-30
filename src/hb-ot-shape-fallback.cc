harfbuzz/harfbuzz

commit 9b0b40b3c1ac8155c80ed5dc976228f4d3ec7e1f

Fix fallback kerning to check for current glyph's mask

src/hb-ot-shape-fallback.cc
@@ -444,7 +444,8 @@ _hb_ot_shape_fallback_kern (const hb_ot_shape_plan_t *plan,
   if (!plan->has_kern) return;
 
   OT::hb_ot_apply_context_t c (1, font, buffer);
-  c.set_lookup_mask (plan->kern_mask);
+  hb_mask_t kern_mask = plan->kern_mask;
+  c.set_lookup_mask (kern_mask);
   c.set_lookup_props (OT::LookupFlag::IgnoreMarks);
   OT::hb_ot_apply_context_t::skipping_iterator_t &skippy_iter = c.iter_input;
   skippy_iter.init (&c);
@@ -454,6 +455,12 @@ _hb_ot_shape_fallback_kern (const hb_ot_shape_plan_t *plan,
   hb_glyph_position_t *pos = buffer->pos;
   for (unsigned int idx = 0; idx < count;)
   {
+    if (!(buffer->cur().mask & kern_mask))
+    {
+      idx++;
+      continue;
+    }
+
     skippy_iter.reset (idx, 1);
     if (!skippy_iter.next ())
     {

commit 5c65ed800de4caef5ee9ad2111225fa5d8235737

Fix bug introduced in 9b0b40b3c1ac8155c80ed5dc976228f4d3ec7e1f
Also discovered by msan bot.

src/hb-ot-shape-fallback.cc
@@ -455,7 +455,7 @@ _hb_ot_shape_fallback_kern (const hb_ot_shape_plan_t *plan,
   hb_glyph_position_t *pos = buffer->pos;
   for (unsigned int idx = 0; idx < count;)
   {
-    if (!(buffer->cur().mask & kern_mask))
+    if (!(info[idx].mask & kern_mask))
     {
       idx++;
       continue;