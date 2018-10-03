674560cf244054a7e8c16073a59aa1b01e1ba5ea
Behdad Esfahbod <behdad@behdad.org>
2018-10-03 19:10:28 +0200

    Fix build

--- src/test-unicode-ranges.cc
+++ src/test-unicode-ranges.cc
@@ -31,10 +31,10 @@
 static void
 test (hb_codepoint_t cp, unsigned int bit)
 {
-  if (OT::hb_get_unicode_range_bit (cp) != bit)
+  if (OT::_hb_ot_os2_get_unicode_range_bit (cp) != bit)
   {
     fprintf (stderr, "got incorrect bit (%d) for cp 0x%X. Should have been %d.",
-             OT::hb_get_unicode_range_bit (cp),
+             OT::_hb_ot_os2_get_unicode_range_bit (cp),
              cp,
              bit);
     abort();
