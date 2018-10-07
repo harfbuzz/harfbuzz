36672eff43e31e1bc0361869dbf7cedbc604f565
Ebrahim Byagowi <ebrahim@gnu.org>
2018-10-07 11:25:23 +0330

    Enable TRAK applying and add a test

--- src/hb-ot-shape.cc
+++ src/hb-ot-shape.cc
@@ -849,7 +849,7 @@ hb_ot_position (hb_ot_shape_context_t *c)
 
   _hb_buffer_deallocate_gsubgpos_vars (c->buffer);
 
-  //hb_aat_layout_position (c->font, c->buffer);
+  hb_aat_layout_position (c->font, c->buffer);
 }
 
 static inline void
