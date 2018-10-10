7e6e5bf6147596d6d096e2ba37f3a6eefd7429cd
Behdad Esfahbod <behdad@behdad.org>
2018-10-10 18:59:07 -0400

    Fix option string matching

--- src/hb-common.cc
+++ src/hb-common.cc
@@ -57,7 +57,7 @@ _hb_options_init (void)
         p = c + strlen (c);
 
 #define OPTION(name, symbol) \
-	if (0 == strncmp (c, name, p - c)) u.opts.symbol = true;
+	if (0 == strncmp (c, name, p - c) && strlen (name) == p - c) u.opts.symbol = true;
 
       OPTION ("uniscribe-bug-compatible", uniscribe_bug_compatible);
       OPTION ("aat", aat);
