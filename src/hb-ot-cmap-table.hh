0e2680a6e8d16afd38ffdb6e8cf244150a5e3837
Behdad Esfahbod <behdad@behdad.org>
2018-11-11 00:28:47 -0500

    [cmap] Port to hb_blob_ptr_t
    Although didn't need it...

--- src/hb-ot-cmap-table.hh
+++ src/hb-ot-cmap-table.hh
@@ -1014,8 +1014,7 @@ struct cmap
   {
     inline void init (hb_face_t *face)
     {
-      this->blob = hb_sanitize_context_t().reference_table<cmap> (face);
-      const cmap *table = this->blob->as<cmap> ();
+      this->table = hb_sanitize_context_t().reference_table<cmap> (face);
       bool symbol;
       this->subtable = table->find_best_subtable (&symbol);
       this->subtable_uvs = &Null(CmapSubtableFormat14);
@@ -1051,7 +1050,7 @@ struct cmap
 
     inline void fini (void)
     {
-      hb_blob_destroy (this->blob);
+      hb_blob_destroy (this->table.get_blob ());
     }
 
     inline bool get_nominal_glyph (hb_codepoint_t  unicode,
@@ -1157,7 +1156,7 @@ struct cmap
 
     CmapSubtableFormat4::accelerator_t format4_accel;
 
-    hb_blob_t *blob;
+    hb_blob_ptr_t<cmap> table;
   };
 
   protected:
