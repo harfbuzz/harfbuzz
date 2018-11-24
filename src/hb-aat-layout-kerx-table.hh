harfbuzz/harfbuzz

commit 8dcc1913a1670ede7b124f7b5b775d7ab8791386

[kerx/morx] Make sure object length is sanitized before accessing it

src/hb-aat-layout-kerx-table.hh
@@ -962,6 +962,11 @@ struct KerxTable
     unsigned int count = thiz()->tableCount;
     for (unsigned int i = 0; i < count; i++)
     {
+      if (unlikely (!st->u.header.sanitize (c)))
+      {
+	c->reset_object ();
+	return_trace (false);
+      }
       /* OpenType kern table has 2-byte subtable lengths.  That's limiting.
        * MS implementation also only supports one subtable, of format 0,
        * anyway.  Certain versions of some fonts, like Calibry, contain

src/hb-aat-layout-morx-table.hh
@@ -1061,6 +1061,11 @@ struct Chain
     unsigned int count = subtableCount;
     for (unsigned int i = 0; i < count; i++)
     {
+      if (unlikely (!c->check_struct (subtable)))
+      {
+	c->reset_object ();
+	return_trace (false);
+      }
       c->set_object (*subtable);
       if (!subtable->sanitize (c))
 	return_trace (false);

commit 20edc70d537b01e8a384ee05673335f2f8a18238

[morx/kerx] Fix sanitize regression
Broke in 8dcc1913a1670ede7b124f7b5b775d7ab8791386

If sanitizer is left with another object, it wouldn't work.

Better fix coming soon.

src/hb-aat-layout-kerx-table.hh
@@ -962,11 +962,9 @@ struct KerxTable
     unsigned int count = thiz()->tableCount;
     for (unsigned int i = 0; i < count; i++)
     {
+      c->reset_object ();
       if (unlikely (!st->u.header.sanitize (c)))
-      {
-	c->reset_object ();
 	return_trace (false);
-      }
       /* OpenType kern table has 2-byte subtable lengths.  That's limiting.
        * MS implementation also only supports one subtable, of format 0,
        * anyway.  Certain versions of some fonts, like Calibry, contain
@@ -976,8 +974,6 @@ struct KerxTable
        * the length for the last subtable. */
       if (i < count - 1)
 	c->set_object (*st);
-      else
-	c->reset_object ();
 
       if (unlikely (!st->sanitize (c)))
 	return_trace (false);

src/hb-aat-layout-morx-table.hh
@@ -1061,11 +1061,9 @@ struct Chain
     unsigned int count = subtableCount;
     for (unsigned int i = 0; i < count; i++)
     {
+      c->reset_object ();
       if (unlikely (!c->check_struct (subtable)))
-      {
-	c->reset_object ();
 	return_trace (false);
-      }
       c->set_object (*subtable);
       if (!subtable->sanitize (c))
 	return_trace (false);