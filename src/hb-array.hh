7c0e3e9b2b077fced829a10f616ed3d6b51c15c4
Behdad Esfahbod <behdad@behdad.org>
2018-12-16 23:43:17 -0500

    [array] Add constructor from hb_array_t<const Type>

--- src/hb-array.hh
+++ src/hb-array.hh
@@ -41,6 +41,7 @@ struct hb_array_t
 
   hb_array_t (void) : arrayZ (nullptr), len (0) {}
   hb_array_t (const hb_array_t &o) : arrayZ (o.arrayZ), len (o.len) {}
+  hb_array_t (hb_array_t<const Type> o) : arrayZ (o.arrayZ), len (o.len) {}
   hb_array_t (Type *array_, unsigned int len_) : arrayZ (array_), len (len_) {}
 
   Type& operator [] (int i_) const
