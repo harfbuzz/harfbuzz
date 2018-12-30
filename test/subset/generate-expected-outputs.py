b5c12b9f4d2c32a5013c340f7e40649cb5f2b41a
cclauss <cclauss@me.com>
2018-12-30 13:07:28 +0100

    print() is a function in Python 3 (#1520)

--- test/subset/generate-expected-outputs.py
+++ test/subset/generate-expected-outputs.py
@@ -3,6 +3,8 @@
 # Pre-generates the expected output subset files (via fonttools) for
 # specified subset test suite(s).
 
+from __future__ import print_function, division, absolute_import
+
 import io
 import os
 import sys
@@ -12,7 +14,7 @@ from subset_test_suite import SubsetTestSuite
 
 
 def usage():
-	print "Usage: generate-expected-outputs.py <test suite file> ..."
+	print("Usage: generate-expected-outputs.py <test suite file> ...")
 
 
 def generate_expected_output(input_file, unicodes, profile_flags, output_path):
@@ -37,11 +39,11 @@ for path in args:
 		test_suite = SubsetTestSuite(path, f.read())
 		output_directory = test_suite.get_output_directory()
 
-		print "Generating output files for %s" % output_directory
+		print("Generating output files for %s" % output_directory)
 		for test in test_suite.tests():
 			unicodes = test.unicodes()
 			font_name = test.get_font_name()
-			print "Creating subset %s/%s" % (output_directory, font_name)
+			print("Creating subset %s/%s" % (output_directory, font_name))
 			generate_expected_output(test.font_path, unicodes, test.get_profile_flags(),
 						 os.path.join(output_directory,
 							      font_name))
