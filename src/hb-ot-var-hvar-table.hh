f84342c094727a41be6225d484d30170d1ddf118
Michiharu Ariza <ariza@adobe.com>
2019-03-28 16:17:36 -0700

    minor

--- src/hb-ot-var-hvar-table.hh
+++ src/hb-ot-var-hvar-table.hh
@@ -182,7 +182,7 @@ struct index_map_subset_plan_t
       last_gid = gid;
     }
 
-    if (last_gid == (hb_codepoint_t)-1) return;
+    if (unlikely (last_gid == (hb_codepoint_t)-1)) return;
     map_count = last_gid;
     for (gid = 0; gid < map_count; gid++)
     {
