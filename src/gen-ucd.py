b71d353cee89a6654810f75e7a1d7fd156b76faa
Behdad Esfahbod <behdad@behdad.org>
2019-05-20 17:33:55 -0400

    [gen-ucd] Remove some code

--- src/gen-ucd.py
+++ src/gen-ucd.py
@@ -22,9 +22,7 @@ dm = {i:tuple(int(v, 16) for v in u['dm'].split()) for i,u in enumerate(ucd)
 gc_set = set(gc)
 gc_ccc_non0 = set((cat,klass) for cat,klass in zip(gc,ccc) if klass)
 gc_bmg_non0 = set((cat,mirr) for cat,mirr in zip(gc, bmg) if mirr)
-sc_set = set(sc)
 dm2 = set(v for v in dm.values() if len(v) == 2)
-dm2diff = set(v[1] - v[0] for v in dm2)
 dm1 = set(v[0] for i,v in dm.items() if len(v) == 1)
 dmx = set(v for v in dm.values() if len(v) not in (1,2))
 assert not dmx
@@ -35,14 +33,9 @@ assert not dmx
 #print("GC, CCC, and BMG fit in one byte. Compress together.")
 #print()
 
-#print(len(sorted(sc_set)))
-#print("SC fits in one byte.  Compress separately.")
-#print()
-
 #print(len(dm))
 #print(len(dm1), min(dm1), max(dm1))
 #print(len(dm2))
-##print(sorted(dm2diff))
 #print(len(sorted(set(v // 512 for v in dm1))))
 
 gc_order = packTab.AutoMapping()
