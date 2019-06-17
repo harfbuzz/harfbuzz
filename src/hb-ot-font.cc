eb2825c7f140185f41922a371434873a0114ef67
Behdad Esfahbod <behdad@behdad.org>
2019-06-17 13:42:53 -0700

    Minor

--- src/hb-ot-font.cc
+++ src/hb-ot-font.cc
@@ -37,7 +37,6 @@
 #include "hb-ot-cff1-table.hh"
 #include "hb-ot-cff2-table.hh"
 #include "hb-ot-hmtx-table.hh"
-#include "hb-ot-kern-table.hh"
 #include "hb-ot-os2-table.hh"
 #include "hb-ot-post-table.hh"
 #include "hb-ot-stat-table.hh" // Just so we compile it; unused otherwise.
