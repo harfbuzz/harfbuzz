9f31836c1c84a9b46eccef0b4822d264c487c4d6
Simon Sapin <simon.sapin@exyr.org>
2019-07-04 23:05:50 +0200

    hb_set_previous_range docs: fix presumed copy/paste error

--- src/hb-set.cc
+++ src/hb-set.cc
@@ -524,7 +524,7 @@ hb_set_next_range (const hb_set_t *set,
  * @last: (out): output last codepoint in the range.
  *
  * Gets the previous consecutive range of numbers in @set that
- * are greater than current value of @last.
+ * are less than current value of @first.
  *
  * Set @first to %HB_SET_VALUE_INVALID to get started.
  *
