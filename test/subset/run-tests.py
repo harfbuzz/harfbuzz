6e35668b452cef86702e2563f4006a1db5d8fd74
blueshade7 <ariza@typekit.com>
2019-07-09 09:47:54 -0700

    fix var-subset build

--- test/subset/run-tests.py
+++ test/subset/run-tests.py
@@ -67,7 +67,7 @@ def run_test (test, should_check_ots):
 		    "--font-file=" + test.font_path,
 		    "--output-file=" + out_file,
 		    "--unicodes=%s" % test.unicodes (),
-		    "--drop-tables+=DSIG,GPOS,GSUB,GDEF"]
+		    "--drop-tables+=DSIG,GPOS,GSUB,GDEF,HVAR,VVAR,gvar"]
 	cli_args.extend (test.get_profile_flags ())
 	print (' '.join (cli_args))
 	_, return_code = cmd (cli_args)
