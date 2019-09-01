1f3a2852bd106c6578e0cd5216e59a165ed82f3b
Ebrahim Byagowi <ebrahim@gnu.org>
2019-09-02 00:02:06 +0430

    [subset] Run once for a tag

--- src/hb-subset.cc
+++ src/hb-subset.cc
@@ -285,21 +285,24 @@ hb_subset (hb_face_t *source,
   hb_tag_t table_tags[32];
   unsigned int offset = 0, count;
   bool success = true;
+  hb_set_t *tags_set = hb_set_create ();
   do {
     count = ARRAY_LENGTH (table_tags);
     hb_face_get_table_tags (source, offset, &count, table_tags);
     for (unsigned int i = 0; i < count; i++)
     {
       hb_tag_t tag = table_tags[i];
-      if (_should_drop_table (plan, tag))
+      if (_should_drop_table (plan, tag) && !hb_set_has (tags_set, tag))
       {
 	DEBUG_MSG(SUBSET, nullptr, "drop %c%c%c%c", HB_UNTAG (tag));
 	continue;
       }
+      hb_set_add (tags_set, tag);
       success = success && _subset_table (plan, tag);
     }
     offset += count;
   } while (success && count == ARRAY_LENGTH (table_tags));
+  hb_set_destroy (tags_set);
 
   hb_face_t *result = success ? hb_face_reference (plan->dest) : hb_face_get_empty ();
   hb_subset_plan_destroy (plan);
