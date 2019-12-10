b7d01281d29c05b7b08d0757d380067dd28e0cfb
Behdad Esfahbod <behdad@behdad.org>
2019-12-09 21:59:00 -0600

    [simd] Add disabled code for preferring bsearch for shorter lists

--- src/hb-array.hh
+++ src/hb-array.hh
@@ -374,6 +374,15 @@ struct hb_sorted_array_t :
 		(unsigned) this->arrayZ[i].last);
      }
 #endif
+#if 0
+    if (likely (this->length < 81))
+      return hb_bsearch_impl (pos,
+			      x,
+			      this->arrayZ,
+			      this->length,
+			      sizeof (Type),
+			      _hb_cmp_method<hb_codepoint_t, Type>);
+#endif
     return hb_simd_ksearch_glyphid_range (pos,
 					  x,
 					  this->arrayZ,
