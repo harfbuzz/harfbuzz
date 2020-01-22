1b107086bbb21635743159cd7abadce83f41f69f
ariza <ariza@adobe.com>
2020-01-22 12:28:30 -0800

    add api test

--- test/api/test-subset-gpos.c
+++ test/api/test-subset-gpos.c
@@ -54,12 +54,39 @@ test_subset_gpos_lookup_subtable (void)
   hb_face_destroy (face_wa);
 }
 
+/* TODO: Once GDEF subsetting is implemented, this test may fail & expected result need update. */
+static void
+test_subset_gpos_pairpos1_vf (void)
+{
+  hb_face_t *face_wav = hb_test_open_font_file ("fonts/AdobeVFPrototype.WAV.gpos.otf");
+  hb_face_t *face_wa = hb_test_open_font_file ("fonts/AdobeVFPrototype.WA.gpos.otf");
+
+  hb_set_t *codepoints = hb_set_create ();
+  hb_face_t *face_wav_subset;
+  hb_set_add (codepoints, 'W');
+  hb_set_add (codepoints, 'A');
+
+  hb_subset_input_t *input = hb_subset_test_create_input (codepoints);
+
+  hb_set_del (hb_subset_input_drop_tables_set (input), HB_TAG ('G', 'P', 'O', 'S'));
+
+  face_wav_subset = hb_subset_test_create_subset (face_wav, input);
+  hb_set_destroy (codepoints);
+
+  hb_subset_test_check (face_wa, face_wav_subset, HB_TAG ('G','P','O','S'));
+
+  hb_face_destroy (face_wav_subset);
+  hb_face_destroy (face_wav);
+  hb_face_destroy (face_wa);
+}
+
 int
 main (int argc, char **argv)
 {
   hb_test_init (&argc, &argv);
 
   hb_test_add (test_subset_gpos_lookup_subtable);
+  hb_test_add (test_subset_gpos_pairpos1_vf);
 
   return hb_test_run ();
 }
