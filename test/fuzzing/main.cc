96b71e802fe8d1fa9a14d1dece7935f4eb9789af
Ebrahim Byagowi <ebrahim@gnu.org>
2020-02-24 23:00:58 +0330

    [fuzz] make the custom loader to handle multiple files
    Actually this was the way it used to work :)

--- test/fuzzing/main.cc
+++ test/fuzzing/main.cc
@@ -1,28 +1,25 @@
 #include "hb-fuzzer.hh"
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
 
 int main (int argc, char **argv)
 {
-  hb_blob_t *blob = hb_blob_create_from_file (argv[1]);
-
-  unsigned int len;
-  const char *font_data = hb_blob_get_data (blob, &len);
-  if (len == 0)
-  {
-    printf ("Font not found.\n");
-    exit (1);
-  }
-
   for (int i = 1; i < argc; i++)
   {
+    hb_blob_t *blob = hb_blob_create_from_file (argv[i]);
+
+    unsigned int len;
+    const char *font_data = hb_blob_get_data (blob, &len);
+    if (len == 0)
+    {
+      printf ("Font not found.\n");
+      return 1;
+    }
+
     printf ("%s\n", argv[i]);
     LLVMFuzzerTestOneInput ((const uint8_t *) font_data, len);
+
+    hb_blob_destroy (blob);
   }
-
-  hb_blob_destroy (blob);
-
   return 0;
 }
