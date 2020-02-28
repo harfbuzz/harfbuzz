7496319f41fb8a76206a588557e653d9b8cf1835
Ebrahim Byagowi <ebrahim@gnu.org>
2020-02-28 22:54:57 +0330

    [glyf] Don't accept gids higher that maxp's glyph numbers
    This specially becomes concering on subcomponents where a gvar table
    that is sanitized with maxp's glyph number overflows when a high gid
    accepted here goes sent to it, maybe an additional check be put there
    also, this however feels to be enough.
    
    Fixes https://crbug.com/oss-fuzz/20944

--- src/hb-ot-glyf-table.hh
+++ src/hb-ot-glyf-table.hh
@@ -847,6 +847,7 @@ struct glyf
       glyf_table = hb_sanitize_context_t ().reference_table<glyf> (face);
 
       num_glyphs = hb_max (1u, loca_table.get_length () / (short_offset ? 2 : 4)) - 1;
+      num_glyphs = hb_min (num_glyphs, face->get_num_glyphs ());
     }
 
     void fini ()
