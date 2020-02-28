8545b344889c49325f24f09a8c8b72b2378359f2
Garret Rieger <grieger@google.com>
2020-02-28 14:11:48 -0800

    [subset] Limit the number of features processed in the feature closure.

--- src/hb-ot-layout-gsubgpos.hh
+++ src/hb-ot-layout-gsubgpos.hh
@@ -3104,7 +3104,8 @@ struct GSUBGPOS
   void closure_features (const hb_map_t *lookup_indexes, /* IN */
 			 hb_set_t       *feature_indexes /* OUT */) const
   {
-    for (unsigned i = 0; i < get_feature_count (); i++)
+    unsigned int feature_count = hb_min (get_feature_count (), (unsigned) HB_MAX_FEATURE_INDICES);
+    for (unsigned i = 0; i < feature_count; i++)
     {
       if (get_feature (i).intersects_lookup_indexes (lookup_indexes))
         feature_indexes->add (i);
