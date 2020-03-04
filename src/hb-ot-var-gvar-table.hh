86960378721a70271783069da99a06447fb1e5d9
Ebrahim Byagowi <ebrahim@gnu.org>
2020-03-04 11:15:46 +0330

    [gvar] Make sure TupleVarHeader has the needed size
    Fixes https://crbug.com/oss-fuzz/21026

--- src/hb-ot-var-gvar-table.hh
+++ src/hb-ot-var-gvar-table.hh
@@ -229,7 +229,7 @@ struct GlyphVarData
     {
       return (index < var_data->tupleVarCount.get_count ()) &&
 	     var_data_bytes.check_range (current_tuple, TupleVarHeader::min_size) &&
-	     var_data_bytes.check_range (current_tuple, current_tuple->get_data_size ()) &&
+	     var_data_bytes.check_range (current_tuple, hb_max (current_tuple->get_data_size (), current_tuple->get_size (axis_count))) &&
 	     current_tuple->get_size (axis_count);
     }
 
