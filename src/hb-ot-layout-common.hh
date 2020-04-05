57b7de032f60d0499ae2debb293d0f8456acfdfb
Ebrahim Byagowi <ebrahim@gnu.org>
2020-04-05 17:07:48 +0430

    [subset] Fail ClassDefFormat1 serialization if no space available
    Fixes https://crbug.com/oss-fuzz/21580

--- src/hb-ot-layout-common.hh
+++ src/hb-ot-layout-common.hh
@@ -1651,7 +1651,7 @@ struct ClassDefFormat1
     unsigned glyph_count = glyph_max - glyph_min + 1;
 
     startGlyph = glyph_min;
-    classValue.serialize (c, glyph_count);
+    if (unlikely (!classValue.serialize (c, glyph_count))) return_trace (false);
     for (const hb_pair_t<hb_codepoint_t, unsigned>& gid_klass_pair : + it)
     {
       unsigned idx = gid_klass_pair.first - glyph_min;
