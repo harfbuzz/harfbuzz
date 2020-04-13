ccc235698394aab2cea69bd2e00a0caa766d577b
n8willis <n8willis@users.noreply.github.com>
2020-04-13 15:13:56 +0100

    Update src/hb-graphite2.h
    Co-Authored-By: Khaled Hosny <dr.khaled.hosny@gmail.com>

--- src/hb-graphite2.h
+++ src/hb-graphite2.h
@@ -35,7 +35,7 @@ HB_BEGIN_DECLS
 /**
  * HB_GRAPHITE2_TAG_SILF:
  *
- * The #hb_tag_t tag for the `Silf` table, which holds Graphite2
+ * The #hb_tag_t tag for the `Silf` table, which holds Graphite
  * features. 
  *
  * For more information, see http://graphite.sil.org/
