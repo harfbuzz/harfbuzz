7090707de67e660ae626ecf774f6af8a63a692ab
Behdad Esfahbod <behdad@behdad.org>
2020-06-17 17:41:15 -0700

    [gpos] Break cycle-of-2 when cursive attaching
    Fixes https://github.com/harfbuzz/harfbuzz/issues/2469

--- src/hb-ot-layout-gpos-table.hh
+++ src/hb-ot-layout-gpos-table.hh
@@ -1692,6 +1692,12 @@ struct CursivePosFormat1
     else
       pos[child].x_offset = x_offset;
 
+    /* If parent was attached to child, break them free.
+     * https://github.com/harfbuzz/harfbuzz/issues/2469
+     */
+    if (unlikely (pos[parent].attach_chain() = -pos[child].attach_chain()))
+      pos[parent].attach_chain() = 0;
+
     buffer->idx++;
     return_trace (true);
   }
