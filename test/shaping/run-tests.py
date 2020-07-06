fa73287c7f25f700842f2ba4272a2fcf1c5402c7
Ebrahim Byagowi <ebrahim@gnu.org>
2020-07-06 11:57:07 +0430

    default to 1 for HAVE_FREETYPE

--- test/shaping/run-tests.py
+++ test/shaping/run-tests.py
@@ -15,7 +15,7 @@ if len (args) and args[0] == "--reference":
 	reference = True
 	args = args[1:]
 
-have_freetype = bool(int(os.getenv ('HAVE_FREETYPE', '0')))
+have_freetype = bool(int(os.getenv ('HAVE_FREETYPE', '1')))
 
 if not args or args[0].find('hb-shape') == -1 or not os.path.exists (args[0]):
 	sys.exit ("""First argument does not seem to point to usable hb-shape.""")
