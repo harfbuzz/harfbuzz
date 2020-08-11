16af66c119b5f6a423c95eea175b8180439a75b0
Ebrahim Byagowi <ebrahim@gnu.org>
2020-08-12 02:19:06 +0430

    [fuzzer] Mark alloc_state as unused
    It is really unused when failing-alloc isn't on.

--- test/fuzzing/hb-fuzzer.hh
+++ test/fuzzing/hb-fuzzer.hh
@@ -3,10 +3,16 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
 
+#if defined(__GNUC__) && (__GNUC__ >= 4) || (__clang__)
+#define HB_UNUSED	__attribute__((unused))
+#else
+#define HB_UNUSED
+#endif
+
 #ifdef HB_IS_IN_FUZZER
 /* See src/failing-alloc.c */
 extern "C" int alloc_state;
 #else
 /* Just a dummy global variable */
-static int alloc_state = 0;
+static int HB_UNUSED alloc_state = 0;
 #endif
