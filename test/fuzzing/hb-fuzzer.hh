892fb040dac3dec6e6ecf1b2a97df1f73f0789f9
Ebrahim Byagowi <ebrahim@gnu.org>
2020-08-12 02:30:33 +0430

    [fuzzer] minor

--- test/fuzzing/hb-fuzzer.hh
+++ test/fuzzing/hb-fuzzer.hh
@@ -1,7 +1,7 @@
 #include <hb.h>
 #include <stddef.h>
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
+extern "C" int LLVMFuzzerTestOneInput (const uint8_t *data, size_t size);
 
 #if defined(__GNUC__) && (__GNUC__ >= 4) || (__clang__)
 #define HB_UNUSED	__attribute__((unused))
