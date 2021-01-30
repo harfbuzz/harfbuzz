5cfd56d0fb19b57909d063b857409c0595eb1f17
Behdad Esfahbod <behdad@behdad.org>
2021-01-29 20:36:42 -0700

    [indic] Use lfind()

--- src/hb-ot-shape-complex-indic.hh
+++ src/hb-ot-shape-complex-indic.hh
@@ -310,10 +310,7 @@ static const hb_codepoint_t ra_chars[] = {
 static inline bool
 is_ra (hb_codepoint_t u)
 {
-  for (unsigned int i = 0; i < ARRAY_LENGTH (ra_chars); i++)
-    if (u == ra_chars[i])
-      return true;
-  return false;
+  return hb_array (ra_chars).lfind (u);
 }
 
 static inline void
