99184fbe2b22b2662fdf1b29edd9aabc1abc040b
Khaled Hosny <khaled@aliftype.com>
2021-02-03 13:40:39 +0200

    [ci] Add top level directory to win32 zip file
    Fixes https://github.com/harfbuzz/harfbuzz/issues/2840

--- .ci/build-win32.sh
+++ .ci/build-win32.sh
@@ -11,5 +11,5 @@ cp win32build/util/hb-*.exe win32build/harfbuzz-win32
 find win32build -name '*.dll' -exec cp {} win32build/harfbuzz-win32 \;
 i686-w64-mingw32-strip win32build/harfbuzz-win32/*.{dll,exe}
 rm -f harfbuzz-win32.zip
-(cd win32build/harfbuzz-win32 && zip ../../harfbuzz-win32.zip -r .)
+(cd win32build && zip -r ../harfbuzz-win32.zip harfbuzz-win32)
 echo "harfbuzz-win32.zip is ready."
