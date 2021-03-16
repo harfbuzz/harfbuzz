03538e872a0610a65fad692b33d3646f387cf578
Khaled Hosny <khaled@aliftype.com>
2021-03-16 19:28:35 +0200

    2.8.0

--- src/hb-version.h
+++ src/hb-version.h
@@ -47,20 +47,20 @@ HB_BEGIN_DECLS
  *
  * The minor component of the library version available at compile-time.
  */
-#define HB_VERSION_MINOR 7
+#define HB_VERSION_MINOR 8
 /**
  * HB_VERSION_MICRO:
  *
  * The micro component of the library version available at compile-time.
  */
-#define HB_VERSION_MICRO 4
+#define HB_VERSION_MICRO 0
 
 /**
  * HB_VERSION_STRING:
  *
  * A string literal containing the library version available at compile-time.
  */
-#define HB_VERSION_STRING "2.7.4"
+#define HB_VERSION_STRING "2.8.0"
 
 /**
  * HB_VERSION_ATLEAST:
