9dc9f0385dce05456d48730bf5dc7cac52eaa4da
Qunxin Liu <qxliu@google.com>
2021-04-08 11:00:17 -0700

    [subset] fix for fuzzer testcase: https://oss-fuzz.com/testcase-detail/5858518134554624

--- src/hb-ot-layout-gpos-table.hh
+++ src/hb-ot-layout-gpos-table.hh
@@ -492,9 +492,13 @@ struct Anchor
   {
     TRACE_SUBSET (this);
     if (c->plan->drop_hints)
+    {
       // AnchorFormat 2 and 3 just containing extra hinting information, so
       // if hints are being dropped convert to format 1.
+      if (u.format != 1 && u.format != 2 && u.format != 3)
+        return_trace (false);
       return_trace (bool (reinterpret_cast<Anchor *> (u.format1.copy (c->serializer))));
+    }
 
     switch (u.format) {
     case 1: return_trace (bool (reinterpret_cast<Anchor *> (u.format1.copy (c->serializer))));
