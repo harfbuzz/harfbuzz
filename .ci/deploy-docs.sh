c7d809cc2d3c83e0e825a58f16ae96e51dfa6c70
Khaled Hosny <khaled@aliftype.com>
2021-06-05 04:06:58 +0200

    Rename various references to master branch

--- .ci/deploy-docs.sh
+++ .ci/deploy-docs.sh
@@ -23,12 +23,12 @@ echo "git remote add upstream \"https://\$GH_TOKEN@github.com/harfbuzz/harfbuzz.
 git remote add upstream "https://$GH_TOKEN@github.com/harfbuzz/harfbuzz.github.io.git"
 set -x
 git fetch upstream
-git reset upstream/master
+git reset upstream/main
 
 touch .
 git add -A .
 
 if [[ $(git status -s) ]]; then
   git commit -m "Rebuild docs for https://github.com/harfbuzz/harfbuzz/commit/$REVISION"
-  git push -q upstream HEAD:master
+  git push -q upstream HEAD:main
 fi
