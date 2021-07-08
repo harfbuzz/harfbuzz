708ba035c68a9bba53eefc08db1479478eb37b38
Khaled Hosny <khaled@aliftype.com>
2021-07-08 22:46:33 +0200

    2.8.2

--- src/hb-set.cc
+++ src/hb-set.cc
@@ -180,7 +180,7 @@ hb_set_allocation_successful (const hb_set_t  *set)
  *
  * Return value: Newly-allocated set.
  *
- * Since: REPLACEME
+ * Since: 2.8.2
  **/
 hb_set_t *
 hb_set_copy (const hb_set_t *set)
