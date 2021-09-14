ad4df20dd62efe6db4993861cba2268bd3921c1a
Behdad Esfahbod <behdad@behdad.org>
2021-09-14 07:11:33 -0400

    [style] Fix float conversion warning

--- src/hb-style.cc
+++ src/hb-style.cc
@@ -49,13 +49,13 @@
 static inline float
 _hb_angle_to_ratio (float a)
 {
-  return tanf (a * (M_PI / 180.f));
+  return tanf (a * float (M_PI / 180.));
 }
 #if 0
 static inline float
 _hb_ratio_to_angle (float r)
 {
-  return atanf (r) * (180.f / M_PI);
+  return atanf (r) * float (180. / M_PI);
 }
 #endif
 
