e78034a38acec6079406676cd92afe4490d29446
Garret Rieger <grieger@google.com>
2022-01-14 16:20:31 -0800

    [map] Correct previous commit, if (...) was reversed.

--- src/hb-map.hh
+++ src/hb-map.hh
@@ -131,7 +131,7 @@ struct hb_hashmap_t
   }
   void fini_shallow ()
   {
-    if (unlikely (!items)) {
+    if (likely (items)) {
       unsigned size = mask + 1;
       for (unsigned i = 0; i < size; i++)
         items[i].~item_t ();
