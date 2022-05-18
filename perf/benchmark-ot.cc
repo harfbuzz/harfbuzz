6e668a2adefdc186dcd300136b3535c43d6fdffd
Behdad Esfahbod <behdad@behdad.org>
2022-05-18 11:16:11 -0600

    [perf/benchmark-ot] Rename test

--- perf/benchmark-ot.cc
+++ perf/benchmark-ot.cc
@@ -27,7 +27,7 @@ static void BM_hb_ot_tags_from_script_and_language (benchmark::State& state,
 					 language_tags /* OUT */);
   }
 }
-BENCHMARK_CAPTURE (BM_hb_ot_tags_from_script_and_language, COMMON zh_trad, HB_SCRIPT_COMMON, "zh_trad");
+BENCHMARK_CAPTURE (BM_hb_ot_tags_from_script_and_language, COMMON zh_abcd, HB_SCRIPT_COMMON, "zh_abcd");
 BENCHMARK_CAPTURE (BM_hb_ot_tags_from_script_and_language, COMMON ab_abcd, HB_SCRIPT_COMMON, "ab_abcd");
 BENCHMARK_CAPTURE (BM_hb_ot_tags_from_script_and_language, COMMON ab_abc, HB_SCRIPT_COMMON, "ab_abc");
 BENCHMARK_CAPTURE (BM_hb_ot_tags_from_script_and_language, COMMON abcdef_XY, HB_SCRIPT_COMMON, "abcdef_XY");
