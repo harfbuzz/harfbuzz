f7f61aeb6fa9a9c9e62727f215d6fa4e55ddb546
Behdad Esfahbod <behdad@behdad.org>
2022-05-31 09:37:38 -0600

    [buffer] Add TODO item

--- src/hb-buffer.cc
+++ src/hb-buffer.cc
@@ -549,6 +549,8 @@ hb_buffer_t::delete_glyph ()
     goto done;
   }
 
+  /* TODO We need to just merge one direction, with whichever is smaller. */
+
   if (out_len)
   {
     /* Merge cluster backward. */
