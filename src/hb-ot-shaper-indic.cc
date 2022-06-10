39c132a62d57d736971fe16e1e47817e443df4f0
Behdad Esfahbod <behdad@behdad.org>
2022-06-10 07:12:39 -0600

    [indic/myanmar] Simplify compare functions

--- src/hb-ot-shaper-indic.cc
+++ src/hb-ot-shaper-indic.cc
@@ -461,7 +461,7 @@ compare_indic_order (const hb_glyph_info_t *pa, const hb_glyph_info_t *pb)
   int a = pa->indic_position();
   int b = pb->indic_position();
 
-  return a < b ? -1 : a == b ? 0 : +1;
+  return (int) a - (int) b;
 }
 
 
--- src/hb-ot-shaper-myanmar.cc
+++ src/hb-ot-shaper-myanmar.cc
@@ -202,7 +202,7 @@ compare_myanmar_order (const hb_glyph_info_t *pa, const hb_glyph_info_t *pb)
   int a = pa->myanmar_position();
   int b = pb->myanmar_position();
 
-  return a < b ? -1 : a == b ? 0 : +1;
+  return (int) a - (int) b;
 }
 
 
