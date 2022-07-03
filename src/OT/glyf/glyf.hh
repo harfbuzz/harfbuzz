115e1a03e7612a888ed248505ef5bd25a55eedb1
Behdad Esfahbod <behdad@behdad.org>
2022-07-03 14:45:12 -0600

    [glyf] Relax condition for matching number of coords

--- src/OT/glyf/glyf.hh
+++ src/OT/glyf/glyf.hh
@@ -265,7 +265,7 @@ struct glyf_accelerator_t
     bool success = false;
 
     contour_point_t phantoms[glyf_impl::PHANTOM_COUNT];
-    if (likely (font->num_coords == gvar->get_axis_count ()))
+    if (font->num_coords)
       success = get_points (font, gid, points_aggregator_t (font, nullptr, phantoms, false));
 
     if (unlikely (!success))
