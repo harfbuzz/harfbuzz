5d18d1835aaff68a43961d5a92a43953d47537a3
Garret Rieger <grieger@google.com>
2022-07-07 20:16:16 +0000

    [subset] cache sanitized tables in subset plan to avoid sanitizing tables multiple times.

--- src/hb-subset-plan.hh
+++ src/hb-subset-plan.hh
@@ -106,7 +106,28 @@ struct hb_subset_plan_t
   //Old -> New layout item variation store delta set index mapping
   hb_map_t *layout_variation_idx_map;
 
+  hb_hashmap_t<hb_tag_t, hb::unique_ptr<hb_blob_t>>* sanitized_table_cache;
+
  public:
+
+  template<typename T>
+  hb_blob_ptr_t<T> source_table()
+  {
+    if (sanitized_table_cache
+        && !sanitized_table_cache->in_error ()
+        && sanitized_table_cache->has (T::tableTag)) {
+      return hb_blob_reference (sanitized_table_cache->get (T::tableTag).get ());
+    }
+
+    hb::unique_ptr<hb_blob_t> table_blob {hb_sanitize_context_t ().reference_table<T> (source)};
+    hb_blob_t* ret = hb_blob_reference (table_blob.get ());
+
+    if (likely (sanitized_table_cache))
+      sanitized_table_cache->set (T::tableTag,
+                                  std::move (table_blob));
+
+    return ret;
+  }
 
   bool in_error () const { return !successful; }
 
