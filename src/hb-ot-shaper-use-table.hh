826639fab2b213af1dc5c00d3b1ab5b5187e30fe
Behdad Esfahbod <behdad@behdad.org>
2022-08-02 12:18:18 -0600

    [src] Add test-use-table
    Fixes https://github.com/harfbuzz/harfbuzz/issues/3758

--- src/hb-ot-shaper-use-table.hh
+++ src/hb-ot-shaper-use-table.hh
@@ -393,26 +393,5 @@ hb_use_get_category (unsigned u)
 #undef VMPre
 
 
-#ifdef HB_USE_TABLE_MAIN
-int main (int argc, char **argv)
-{
-  if (argc != 2)
-  {
-    for (unsigned u = 0; u < 0x10FFFFu; u++)
-      printf ("U+%04X %d\n", u, hb_use_get_category (u));
-    return 0;
-  }
-
-  hb_codepoint_t u;
-  sscanf (argv[1], "%x", &u);
-
-  printf ("%d\n", hb_use_get_category (u));
-
-  return 0;
-}
-
-#endif
-
-
 #endif /* HB_OT_SHAPER_USE_TABLE_HH */
 /* == End of generated table == */
