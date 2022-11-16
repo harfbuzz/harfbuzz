443961971adcfd1a8a2c1d3e8a3aeb8a49297bed
Behdad Esfahbod <behdad@behdad.org>
2022-11-16 16:35:00 -0700

    [perf] Add fa-words.txt and use in shape benchmark

--- perf/benchmark-shape.cc
+++ perf/benchmark-shape.cc
@@ -27,6 +27,10 @@ struct test_input_t
    "perf/texts/fa-thelittleprince.txt",
    false},
 
+  {"perf/fonts/NotoNastaliqUrdu-Regular.ttf",
+   "perf/texts/fa-words.txt",
+   false},
+
   {"perf/fonts/Amiri-Regular.ttf",
    "perf/texts/fa-thelittleprince.txt",
    false},
