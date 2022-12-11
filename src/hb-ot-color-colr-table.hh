d0ee5a452f057386d5dbffef1e8f7912fec8537c
Behdad Esfahbod <behdad@behdad.org>
2022-12-11 13:13:35 -0700

    [COLR] Return false from get_extents if table version not 1

--- src/hb-ot-color-colr-table.hh
+++ src/hb-ot-color-colr-table.hh
@@ -1547,6 +1547,8 @@ struct COLR
   bool
   get_extents (hb_font_t *font, hb_codepoint_t glyph, hb_glyph_extents_t *extents) const
   {
+    if (version != 1)
+      return false;
     if ((this+clipList).get_extents (glyph, extents))
     {
       extents->x_bearing = font->em_scale_x (extents->x_bearing);
