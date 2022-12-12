6bb478eeeb665ddb1801a424679644b5b2b6c9e1
Behdad Esfahbod <behdad@behdad.org>
2022-12-12 11:39:06 -0700

    [VarC] Clamp after addition

--- src/OT/glyf/VarCompositeGlyph.hh
+++ src/OT/glyf/VarCompositeGlyph.hh
@@ -248,8 +248,9 @@ struct VarCompositeGlyphRecord
     {
       unsigned axis_index = axis_width == 1 ? *p++ : *q++;
       signed v = axis_points[i].x;
+      v += setter[axis_index];
       v = hb_clamp (v, -(1<<14), (1<<14));
-      setter[axis_index] += v;
+      setter[axis_index] = v;
     }
   }
 
