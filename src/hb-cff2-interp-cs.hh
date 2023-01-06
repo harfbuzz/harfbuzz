76879c5763643bf83680efbc610fede3d9faab00
Behdad Esfahbod <behdad@behdad.org>
2023-01-06 11:47:04 -0700

    [subset-cff] Minor hide num_coords again

--- src/hb-cff2-interp-cs.hh
+++ src/hb-cff2-interp-cs.hh
@@ -163,10 +163,11 @@ struct cff2_cs_interp_env_t : cs_interp_env_t<ELEM, CFF2Subrs>
     return v;
   }
 
-  public:
+  bool have_coords () const { return num_coords; }
+
+  protected:
   const int     *coords;
   unsigned int  num_coords;
-  protected:
   const	 CFF2VariationStore *varStore;
   unsigned int  region_count;
   unsigned int  ivs;
@@ -223,7 +224,7 @@ struct cff2_cs_opset_t : cs_opset_t<ELEM, OPSET, cff2_cs_interp_env_t<ELEM>, PAR
 				 const hb_array_t<const ELEM> blends,
 				 unsigned n, unsigned i)
   {
-    if (env.num_coords)
+    if (env.have_coords ())
       arg.set_int (round (arg.to_real () + env.blend_deltas (blends)));
     else
       arg.set_blends (n, i, blends);
