3c734f4479068c0b9a6a768ff55d9ecd0d73d6f7
Behdad Esfahbod <behdad@behdad.org>
2023-01-19 15:27:13 -0700

    [hb-info] Add --show-postscript-name

--- util/hb-info.cc
+++ util/hb-info.cc
@@ -45,6 +45,7 @@ struct info_t
       {"show-style",	0, 0, G_OPTION_ARG_NONE,	&this->show_style,		"Show style name",		nullptr},
       {"show-unique-name",0, 0, G_OPTION_ARG_NONE,	&this->show_unique_name,	"Show unique name",		nullptr},
       {"show-full-name",0, 0, G_OPTION_ARG_NONE,	&this->show_full_name,		"Show full name",		nullptr},
+      {"show-postscript-name",0, 0, G_OPTION_ARG_NONE,	&this->show_postscript_name,	"Show Postscript name",		nullptr},
       {"show-upem",	0, 0, G_OPTION_ARG_NONE,	&this->show_upem,		"Show Units-Per-EM",		nullptr},
       {"show-unicode-count",0, 0, G_OPTION_ARG_NONE,	&this->show_unicode_count,	"Show Unicode count",		nullptr},
       {"show-glyph-count",0, 0, G_OPTION_ARG_NONE,	&this->show_glyph_count,	"Show glyph count",		nullptr},
@@ -83,6 +84,7 @@ struct info_t
   hb_bool_t show_style = false;
   hb_bool_t show_unique_name = false;
   hb_bool_t show_full_name = false;
+  hb_bool_t show_postscript_name = false;
   hb_bool_t show_upem = false;
   hb_bool_t show_unicode_count = false;
   hb_bool_t show_glyph_count = false;
@@ -120,6 +122,7 @@ struct info_t
       show_style =
       show_unique_name =
       show_full_name =
+      show_postscript_name =
       show_upem =
       show_unicode_count =
       show_glyph_count =
@@ -145,8 +148,9 @@ struct info_t
     if (show_style)	  _show_style ();
     if (show_unique_name) _show_unique_name ();
     if (show_full_name)	  _show_full_name ();
+    if (show_postscript_name)_show_postscript_name ();
     if (show_upem)	  _show_upem ();
-    if (show_unicode_count) _show_unicode_count ();
+    if (show_unicode_count)_show_unicode_count ();
     if (show_glyph_count) _show_glyph_count ();
 
     if (list_names)	  _list_names ();
@@ -196,6 +200,7 @@ struct info_t
   void _show_style ()		{ _show_name ("Style", 2); }
   void _show_unique_name ()	{ _show_name ("Unique name", 3); }
   void _show_full_name ()	{ _show_name ("Full name", 4); }
+  void _show_postscript_name ()	{ _show_name ("Postscript name", 6); }
 
   void _show_upem ()
   {
