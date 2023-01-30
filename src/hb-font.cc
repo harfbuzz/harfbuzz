e7a71ea15b1df6feb3ca9811eb3abe721a63e21f
Behdad Esfahbod <behdad@behdad.org>
2023-01-30 11:21:08 -0700

    [font] Docs

--- src/hb-font.cc
+++ src/hb-font.cc
@@ -1061,7 +1061,8 @@ hb_font_get_nominal_glyph (hb_font_t      *font,
  * @glyph_stride: The stride between successive glyph IDs
  *
  * Fetches the nominal glyph IDs for a sequence of Unicode code points. Glyph
- * IDs must be returned in a #hb_codepoint_t output parameter.
+ * IDs must be returned in a #hb_codepoint_t output parameter. Stopes at the
+ * first unsupported glyph ID.
  *
  * Return value: the number of code points processed
  *
