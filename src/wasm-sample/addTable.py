5ab7f7a7d41720bb8ea189df0c5c47db1a22d550
Behdad Esfahbod <behdad@behdad.org>
2023-02-23 16:11:54 -0700

    [wasm-sample] Make addTable take args

--- src/wasm-sample/addTable.py
+++ src/wasm-sample/addTable.py
@@ -1,11 +1,16 @@
+import sys
 from fontTools.ttLib import TTFont
 from fontTools.ttLib.tables.DefaultTable import DefaultTable
 
-font = TTFont("test.ttf")
+if len(sys.argv) == 1:
+    print("usage: python addTable.py input.ttf output.ttf Wasm.bin")
+    sys.exit(1)
+
+font = TTFont(sys.argv[1])
 
 wasm_table = DefaultTable("Wasm")
-wasm_table.data = open("shape.so", "rb").read()
+wasm_table.data = open(sys.argv[3], "rb").read()
 
 font["Wasm"] = wasm_table
 
-font.save("test.wasm.ttf")
+font.save(sys.argv[2])
