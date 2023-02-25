83b9c34f0b5d9b6b6f65aae0e1bb92877972a5ed
Simon Cozens <simon@simon-cozens.org>
2023-02-25 15:23:22 +0000

    [wasm] Add rust example

---
+++ src/wasm/sample/rust/hello-wasm/src/lib.rs
@@ -0,0 +1,18 @@
+use harfbuzz_wasm::{Font, GlyphBuffer};
+use tiny_rng::{Rand, Rng};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn shape(font_ref: u32, buf_ref: u32) -> i32 {
+    let mut rng = Rng::from_seed(123456);
+    let font = Font::from_ref(font_ref);
+    font.shape_with(buf_ref, "ot");
+    let mut buffer = GlyphBuffer::from_ref(buf_ref);
+    for mut item in buffer.glyphs.iter_mut() {
+        // Randomize it!
+        item.x_offset = ((rng.rand_u32() as i32) >> 24) - 120;
+        item.y_offset = ((rng.rand_u32() as i32) >> 24) - 120;
+    }
+    // Buffer is written back to HB on drop
+    1
+}
