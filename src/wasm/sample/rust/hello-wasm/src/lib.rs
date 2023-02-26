7e397d8695f32c91bddbf373cd19f4c6e61fa882
Simon Cozens <simon@simon-cozens.org>
2023-02-26 13:57:34 +0000

    [wasm-rust] Fix shape parameters

--- src/wasm/sample/rust/hello-wasm/src/lib.rs
+++ src/wasm/sample/rust/hello-wasm/src/lib.rs
@@ -3,7 +3,13 @@ use tiny_rng::{Rand, Rng};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn shape(font_ref: u32, buf_ref: u32) -> i32 {
+pub fn shape(
+    _shape_plan: u32,
+    font_ref: u32,
+    buf_ref: u32,
+    _features: u32,
+    _num_features: u32,
+) -> i32 {
     let mut rng = Rng::from_seed(123456);
     let font = Font::from_ref(font_ref);
     font.shape_with(buf_ref, "ot");
