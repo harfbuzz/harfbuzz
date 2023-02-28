5c462865920681246422a1d23d38cc62a94f1870
Behdad Esfahbod <behdad@behdad.org>
2023-02-28 12:16:46 -0700

    Revert "."
    This reverts commit 59434578cd453d9dcdcaf5d010d5f5686c17717e.

--- src/hb-shape.h
+++ src/hb-shape.h
@@ -53,15 +53,6 @@ hb_shape_full (hb_font_t          *font,
 	       unsigned int        num_features,
 	       const char * const *shaper_list);
 
-HB_EXTERN hb_bool_t
-hb_shape_justify (hb_font_t          *font,
-		  hb_buffer_t        *buffer,
-		  const hb_feature_t *features,
-		  unsigned int        num_features,
-		  const char * const *shaper_list,
-		  float               target_width,
-		  float              *width);
-
 HB_EXTERN const char **
 hb_shape_list_shapers (void);
 
