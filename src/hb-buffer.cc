92e9f883cf48344ea2f7cd8a63cf820c61a72d98
Jamaika1 <lukaszcz18@wp.pl>
2023-04-29 20:04:17 +0200

    Change defined system

--- src/hb-buffer.cc
+++ src/hb-buffer.cc
@@ -2212,7 +2212,7 @@ hb_buffer_t::message_impl (hb_font_t *font, const char *fmt, va_list ap)
   message_depth++;
 
   char buf[100];
-#if defined(_MSC_VER) || defined(__GNUC__)
+#if defined(__GNUC__)
   vsnprintf_s (buf, sizeof (buf), fmt, ap);
 #else
   vsnprintf (buf, sizeof (buf), fmt, ap);
