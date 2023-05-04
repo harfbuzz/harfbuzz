319a488b3903cf92c3b45d1772717168a10f94d7
Behdad Esfahbod <behdad@behdad.org>
2023-05-04 10:36:48 -0600

    [GDEF] Fix null check
    Fixes https://oss-fuzz.com/testcase-detail/5920994267889664

--- src/OT/Layout/GDEF/GDEF.hh
+++ src/OT/Layout/GDEF/GDEF.hh
@@ -869,7 +869,7 @@ struct GDEF
         return v;
 
       v = table->get_glyph_props (glyph);
-      if (likely (table)) // Don't try setting if we are the null instance!
+      if (likely (table.get_blob ())) // Don't try setting if we are the null instance!
 	glyph_props_cache.set (glyph, v);
 
       return v;
