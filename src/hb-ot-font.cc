5d4f3ff690aa8358b214c90ae7942766d68c12f9
Behdad Esfahbod <behdad@behdad.org>
2023-05-05 07:10:02 -0600

    [TINY] Fix build

--- src/hb-ot-font.cc
+++ src/hb-ot-font.cc
@@ -64,7 +64,9 @@
 using hb_ot_font_cmap_cache_t    = hb_cache_t<21, 16, 8, true>;
 using hb_ot_font_advance_cache_t = hb_cache_t<24, 16, 8, true>;
 
+#ifndef HB_NO_OT_FONT_CMAP_CACHE
 static hb_user_data_key_t hb_ot_font_cmap_cache_user_data_key;
+#endif
 
 struct hb_ot_font_t
 {
