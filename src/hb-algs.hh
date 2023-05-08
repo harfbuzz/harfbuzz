0e026808036f59d3ea4e5954b1a19fcfcb9a9702
Behdad Esfahbod <behdad@behdad.org>
2023-05-08 16:31:58 -0600

    [hash] Add hash impl for integers
    Part of https://github.com/harfbuzz/harfbuzz/issues/4227

--- src/hb-algs.hh
+++ src/hb-algs.hh
@@ -236,7 +236,11 @@ struct
   private:
 
   template <typename T> constexpr auto
-  impl (const T& v, hb_priority<1>) const HB_RETURN (uint32_t, hb_deref (v).hash ())
+  impl (const T& v, hb_priority<2>) const HB_RETURN (uint32_t, hb_deref (v).hash ())
+
+  // Horrible: std:hash() of integers seems to be identity in gcc / clang?!
+  template <typename T> constexpr auto
+  impl (const T& v, hb_priority<1>) const HB_RETURN (uint32_t, v * 2654435761u)
 
   template <typename T> constexpr auto
   impl (const T& v, hb_priority<0>) const HB_RETURN (uint32_t, std::hash<hb_decay<decltype (hb_deref (v))>>{} (hb_deref (v)))
