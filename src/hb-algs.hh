9cc7eb80ffac20cc5cfa90b80bcff2872f6c466b
Behdad Esfahbod <behdad@behdad.org>
2023-05-09 16:13:46 -0600

    [hash] Speed-up int64 hash

--- src/hb-algs.hh
+++ src/hb-algs.hh
@@ -323,7 +323,7 @@ struct
 
   template <typename T,
 	    hb_enable_if (std::is_integral<T>::value && sizeof (T) > sizeof (uint32_t))> constexpr auto
-  impl (const T& v, hb_priority<1>) const HB_RETURN (uint32_t, (v * 8388607u) ^ ((v >> 32) * 8388607u) /* Mersenne prime */)
+  impl (const T& v, hb_priority<1>) const HB_RETURN (uint32_t, (v ^ (v >> 32)) * 8388607u /* Mersenne prime */)
 
   template <typename T> constexpr auto
   impl (const T& v, hb_priority<0>) const HB_RETURN (uint32_t, std::hash<hb_decay<decltype (hb_deref (v))>>{} (hb_deref (v)))
