33507a9c27cb45947e9e1cf6999e69891d0a7ae3
Behdad Esfahbod <behdad@behdad.org>
2023-06-23 12:40:35 -0600

    [subset/cff] Micro-optimize hb_plan_subset_cff_fdselect

--- src/hb-subset-cff-common.cc
+++ src/hb-subset-cff-common.cc
@@ -70,14 +70,14 @@ hb_plan_subset_cff_fdselect (const hb_subset_plan_t *plan,
     hb_codepoint_t prev_fd = CFF_UNDEF_CODE;
     hb_pair_t<unsigned, hb_codepoint_t> last_range {0, 0};
     auto it = hb_iter (plan->new_to_old_gid_list);
+    auto _ = *it;
     for (hb_codepoint_t gid = 0; gid < subset_num_glyphs; gid++)
     {
       hb_codepoint_t old_glyph;
-      auto &_ = *it;
       if (gid == _.first)
       {
 	old_glyph = _.second;
-	it++;
+	_ = *++it;
       }
       else
       {
