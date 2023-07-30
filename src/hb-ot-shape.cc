35a026c89ab4f55a962f0e837a7b7650eb3e7c31
Behdad Esfahbod <behdad@behdad.org>
2023-07-30 11:28:59 -0600

    [shape] Unsafe-to-concat around fraction slash

--- src/hb-ot-shape.cc
+++ src/hb-ot-shape.cc
@@ -766,7 +766,13 @@ hb_ot_shape_setup_masks_fraction (const hb_ot_shape_context_t *c)
 	     HB_UNICODE_GENERAL_CATEGORY_DECIMAL_NUMBER)
 	end++;
       if (start == i || end == i + 1)
+      {
+        if (start == i)
+	  buffer->unsafe_to_concat (start, start + 1);
+	if (end == i + 1)
+	  buffer->unsafe_to_concat (end - 1, end);
 	continue;
+      }
 
       buffer->unsafe_to_break (start, end);
 
