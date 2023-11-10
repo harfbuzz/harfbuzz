9c4d3c3c70d1f4f33294da7ab409c64bdbc12be7
Behdad Esfahbod <behdad@behdad.org>
2023-11-10 12:24:26 -0700

    [graph] Pre-alloc priority-heap array

--- src/graph/graph.hh
+++ src/graph/graph.hh
@@ -567,6 +567,7 @@ struct graph_t
     update_distances ();
 
     hb_priority_queue_t<int64_t> queue;
+    queue.alloc (vertices_.length);
     hb_vector_t<vertex_t> &sorted_graph = vertices_scratch_;
     if (unlikely (!check_success (sorted_graph.resize (vertices_.length)))) return;
     hb_vector_t<unsigned> id_map;
