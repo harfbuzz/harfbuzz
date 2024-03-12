5573ededd2b471a8de95a767d77153ed75140b0e
Qunxin Liu <qxliu@google.com>
2024-03-12 13:33:07 -0700

    [subset] add tests for BASE table subsetting

--- test/subset/run-tests.py
+++ test/subset/run-tests.py
@@ -52,7 +52,7 @@ def run_test (test, should_check_ots, preprocess):
 	cli_args = ["--font-file=" + test.font_path,
 		    "--output-file=" + out_file,
 		    "--unicodes=%s" % test.unicodes (),
-		    "--drop-tables+=DSIG",
+		    "--drop-tables+=DSIG,BASE",
 		    "--drop-tables-=sbix"]
 	if preprocess:
 		cli_args.extend(["--preprocess-face",])
