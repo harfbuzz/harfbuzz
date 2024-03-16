00d56b12aa0b463c6e9fe6c6fbbabc2dd8825a34
Behdad Esfahbod <behdad@behdad.org>
2024-03-16 13:01:49 -0600

    [varc] Apply VarComponent transform
    Works for the first font. Yay!

--- src/OT/Var/VARC/VARC.cc
+++ src/OT/Var/VARC/VARC.cc
@@ -7,6 +7,122 @@ namespace OT {
 //namespace Var {
 
 
+struct hb_transforming_pen_context_t
+{
+  hb_transform_t transform;
+  hb_draw_funcs_t *dfuncs;
+  void *data;
+  hb_draw_state_t *st;
+};
+
+static void
+hb_transforming_pen_move_to (hb_draw_funcs_t *dfuncs HB_UNUSED,
+			     void *data,
+			     hb_draw_state_t *st,
+			     float to_x, float to_y,
+			     void *user_data HB_UNUSED)
+{
+  hb_transforming_pen_context_t *c = (hb_transforming_pen_context_t *) data;
+
+  c->transform.transform_point (to_x, to_y);
+
+  c->dfuncs->move_to (c->data, *c->st, to_x, to_y);
+}
+
+static void
+hb_transforming_pen_line_to (hb_draw_funcs_t *dfuncs HB_UNUSED,
+			     void *data,
+			     hb_draw_state_t *st,
+			     float to_x, float to_y,
+			     void *user_data HB_UNUSED)
+{
+  hb_transforming_pen_context_t *c = (hb_transforming_pen_context_t *) data;
+
+  c->transform.transform_point (to_x, to_y);
+
+  c->dfuncs->line_to (c->data, *c->st, to_x, to_y);
+}
+
+static void
+hb_transforming_pen_quadratic_to (hb_draw_funcs_t *dfuncs HB_UNUSED,
+				  void *data,
+				  hb_draw_state_t *st,
+				  float control_x, float control_y,
+				  float to_x, float to_y,
+				  void *user_data HB_UNUSED)
+{
+  hb_transforming_pen_context_t *c = (hb_transforming_pen_context_t *) data;
+
+  c->transform.transform_point (control_x, control_y);
+  c->transform.transform_point (to_x, to_y);
+
+  c->dfuncs->quadratic_to (c->data, *c->st, control_x, control_y, to_x, to_y);
+}
+
+static void
+hb_transforming_pen_cubic_to (hb_draw_funcs_t *dfuncs HB_UNUSED,
+			      void *data,
+			      hb_draw_state_t *st,
+			      float control1_x, float control1_y,
+			      float control2_x, float control2_y,
+			      float to_x, float to_y,
+			      void *user_data HB_UNUSED)
+{
+  hb_transforming_pen_context_t *c = (hb_transforming_pen_context_t *) data;
+
+  c->transform.transform_point (control1_x, control1_y);
+  c->transform.transform_point (control2_x, control2_y);
+  c->transform.transform_point (to_x, to_y);
+
+  c->dfuncs->cubic_to (c->data, *c->st, control1_x, control1_y, control2_x, control2_y, to_x, to_y);
+}
+
+static void
+hb_transforming_pen_close_path (hb_draw_funcs_t *dfuncs HB_UNUSED,
+				void *data,
+				hb_draw_state_t *st,
+				void *user_data HB_UNUSED)
+{
+  hb_transforming_pen_context_t *c = (hb_transforming_pen_context_t *) data;
+
+  c->dfuncs->close_path (c->data, *c->st);
+}
+
+static inline void free_static_transforming_pen_funcs ();
+
+static struct hb_transforming_pen_funcs_lazy_loader_t : hb_draw_funcs_lazy_loader_t<hb_transforming_pen_funcs_lazy_loader_t>
+{
+  static hb_draw_funcs_t *create ()
+  {
+    hb_draw_funcs_t *funcs = hb_draw_funcs_create ();
+
+    hb_draw_funcs_set_move_to_func (funcs, hb_transforming_pen_move_to, nullptr, nullptr);
+    hb_draw_funcs_set_line_to_func (funcs, hb_transforming_pen_line_to, nullptr, nullptr);
+    hb_draw_funcs_set_quadratic_to_func (funcs, hb_transforming_pen_quadratic_to, nullptr, nullptr);
+    hb_draw_funcs_set_cubic_to_func (funcs, hb_transforming_pen_cubic_to, nullptr, nullptr);
+    hb_draw_funcs_set_close_path_func (funcs, hb_transforming_pen_close_path, nullptr, nullptr);
+
+    hb_draw_funcs_make_immutable (funcs);
+
+    hb_atexit (free_static_transforming_pen_funcs);
+
+    return funcs;
+  }
+} static_transforming_pen_funcs;
+
+static inline
+void free_static_transforming_pen_funcs ()
+{
+  static_transforming_pen_funcs.free_instance ();
+}
+
+static hb_draw_funcs_t *
+hb_transforming_pen_get_funcs ()
+{
+  return static_transforming_pen_funcs.get_unconst ();
+}
+
+
 hb_ubytes_t
 VarComponent::get_path_at (hb_font_t *font, hb_codepoint_t parent_gid, hb_draw_session_t &draw_session,
 			   hb_array_t<const int> coords,
@@ -83,6 +199,8 @@ VarComponent::get_path_at (hb_font_t *font, hb_codepoint_t parent_gid, hb_draw_s
   for (unsigned i = 0; i < axisIndices.length; i++)
     coord_setter[axisIndices[i]] = axisValues[i];
 
+  component_coords = coord_setter.get_coords ();
+
   // Transform
 
   uint32_t transformVarIdx = VarIdx::NO_VARIATION;
@@ -109,12 +227,12 @@ VarComponent::get_path_at (hb_font_t *font, hb_codepoint_t parent_gid, hb_draw_s
 #define PROCESS_TRANSFORM_COMPONENT(type, flag, name) \
 	if (flags & (unsigned) flags_t::flag) \
 	{ \
-	  if (unlikely (record.length < type::static_size)) \
+	  static_assert (type::static_size == HBINT16::static_size, ""); \
+	  if (unlikely (record.length < HBINT16::static_size)) \
 	    return hb_ubytes_t (); \
-	  const type &var = * (const type *) record.arrayZ; \
-	  transform.name = float (var); \
+	  transform.name = * (const HBINT16 *) record.arrayZ; \
 	  transformValuesVector.push (transform.name); \
-	  record += type::static_size; \
+	  record += HBINT16::static_size; \
 	}
   PROCESS_TRANSFORM_COMPONENTS;
 #undef PROCESS_TRANSFORM_COMPONENT
@@ -130,13 +248,41 @@ VarComponent::get_path_at (hb_font_t *font, hb_codepoint_t parent_gid, hb_draw_s
 #undef PROCESS_TRANSFORM_COMPONENT
   }
 
+  // Divide them by their divisors
+#define PROCESS_TRANSFORM_COMPONENT(type, flag, name) \
+	if (flags & (unsigned) flags_t::flag) \
+	{ \
+	  HBINT16 int_v; \
+	  int_v = roundf (transform.name); \
+	  type typed_v = * (const type *) &int_v; \
+	  float float_v = (float) typed_v; \
+	  transform.name = float_v; \
+	}
+  PROCESS_TRANSFORM_COMPONENTS;
+#undef PROCESS_TRANSFORM_COMPONENT
+
   if (!(flags & (unsigned) flags_t::HAVE_SCALE_Y))
     transform.scaleY = transform.scaleX;
 
-  //hb_draw_funcs_t *transformer_funcs = _get_
-  //hb_transformer_context_t context {transform.to_transform (), draw_session};
+  float upem = font->face->upem;
+  float x_scale = font->x_scale / upem;
+  float y_scale = font->y_scale / upem;
 
-  VARC.get_path_at (font, gid, draw_session, coord_setter.get_coords (), parent_gid);
+  transform.translateX *= x_scale;
+  transform.translateY *= y_scale;
+  transform.tCenterX *= x_scale;
+  transform.tCenterY *= y_scale;
+
+  hb_transform_t affine_transform = transform.to_transform ();
+
+  hb_draw_funcs_t *transformer_funcs = hb_transforming_pen_get_funcs ();
+  hb_transforming_pen_context_t context {affine_transform,
+					 draw_session.funcs,
+					 draw_session.draw_data,
+					 &draw_session.st};
+  hb_draw_session_t transformer_session = {transformer_funcs, &context};
+
+  VARC.get_path_at (font, gid, transformer_session, component_coords, parent_gid);
 
   return record;
 }
