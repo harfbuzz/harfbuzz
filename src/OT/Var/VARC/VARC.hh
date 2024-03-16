ef7c0a9b8252f121ce0927c909a38e5cb64b0979
Behdad Esfahbod <behdad@behdad.org>
2024-03-15 21:36:06 -0600

    [varc] Add VarComponent::flags_t

--- src/OT/Var/VARC/VARC.hh
+++ src/OT/Var/VARC/VARC.hh
@@ -16,6 +16,26 @@ namespace OT {
 
 struct VarComponent
 {
+  enum flags_t
+  {
+    RESET_UNSPECIFIED_AXES	= 1u << 0,
+    HAVE_AXES			= 1u << 1,
+    AXIS_VALUES_HAVE_VARIATION	= 1u << 2,
+    TRANSFORM_HAS_VARIATION	= 1u << 3,
+    HAVE_TRANSLATE_X		= 1u << 4,
+    HAVE_TRANSLATE_Y		= 1u << 5,
+    HAVE_ROTATION		= 1u << 6,
+    USE_MY_METRICS		= 1u << 7,
+    HAVE_SCALE_X		= 1u << 8,
+    HAVE_SCALE_Y		= 1u << 9,
+    HAVE_TCENTER_X		= 1u << 10,
+    HAVE_TCENTER_Y		= 1u << 11,
+    GID_IS_24BIT		= 1u << 12,
+    HAVE_SKEW_X			= 1u << 13,
+    HAVE_SKEW_Y			= 1u << 14,
+    RESERVED			= ~((1u << 15) - 1),
+  };
+
   HB_INTERNAL hb_ubytes_t
   get_path_at (hb_font_t *font, hb_codepoint_t parent_gid, hb_draw_session_t &draw_session,
 	       hb_array_t<const int> coords,
@@ -36,6 +56,8 @@ struct VarCompositeGlyph
     }
   }
 };
+
+HB_MARK_AS_FLAG_T (VarComponent::flags_t);
 
 struct VARC
 {
