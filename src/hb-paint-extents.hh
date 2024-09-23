b0cf3d81ea74f41e3fb8b551847c798f46b05633
Behdad Esfahbod <behdad@behdad.org>
2024-09-23 06:50:59 -0600

    [paint] Comment

--- src/hb-paint-extents.hh
+++ src/hb-paint-extents.hh
@@ -70,6 +70,7 @@ struct hb_paint_extents_context_t
     const hb_transform_t &t = transforms.tail ();
     t.transform_extents (extents);
 
+    // TODO Intersect with existing clip?!
     clips.push (hb_bounds_t {extents});
   }
 
