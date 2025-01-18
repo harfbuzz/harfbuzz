89c94925032b8f79006df86eb99cadd76a7da672
Behdad Esfahbod <behdad@behdad.org>
2025-01-17 23:30:56 -0700

    [hvgl] Implement PartShape
    Just struct and sanitize.

--- src/hb-aat-var-hvgl-table.hh
+++ src/hb-aat-var-hvgl-table.hh
@@ -35,6 +35,9 @@ struct coordinates_t
 
   protected:
   HBFLOAT64LE coords[HB_VAR_ARRAY]; // length: SegmentCount * 4
+
+  public:
+  DEFINE_SIZE_MIN (0);
 };
 
 struct deltas_t
@@ -66,6 +69,54 @@ struct deltas_t
 
   protected:
   HBFLOAT64LE matrix[HB_VAR_ARRAY]; // column-major: AxisCount * 2 columns, DeltaSegmentCount * 4 rows
+
+  public:
+  DEFINE_SIZE_MIN (0);
+};
+
+struct PartShape
+{
+  public:
+
+  bool sanitize (hb_sanitize_context_t *c) const
+  {
+    TRACE_SANITIZE (this);
+    if (unlikely (!c->check_struct (this))) return_trace (false);
+    hb_barrier ();
+
+    if (unlikely (!segmentCountPerPath.sanitize (c, pathCount))) return_trace (false);
+    hb_barrier ();
+
+    const auto &blendTypes = StructAfter<decltype (blendTypesX)> (segmentCountPerPath, pathCount);
+    if (unlikely (!blendTypes.sanitize (c, segmentCount))) return_trace (false);
+    hb_barrier ();
+
+    const auto &padding = StructAfter<decltype (paddingX)> (blendTypes, segmentCount);
+    unsigned offset = (const char *) &padding - (const char *) this;
+
+    const auto &coordinates = StructAfter<decltype (coordinatesX)> (padding, offset);
+    if (unlikely (!coordinates.sanitize (c, segmentCount))) return_trace (false);
+    hb_barrier ();
+
+    const auto &deltas = StructAfter<decltype (deltasX)> (coordinates, axisCount, segmentCount);
+    if (unlikely (!deltas.sanitize (c, axisCount, segmentCount))) return_trace (false);
+    hb_barrier ();
+
+    return_trace (true);
+  }
+
+  public:
+  HBUINT16LE flags;
+  HBUINT16LE axisCount;
+  HBUINT16LE pathCount;
+  HBUINT16LE segmentCount;
+  UnsizedArrayOf<HBUINT16LE> segmentCountPerPath; // length: PathCount; Sizes of paths
+  UnsizedArrayOf<HBUINT8> blendTypesX; // length: SegmentCount; Blend types for all segments
+  Align<8> paddingX; // Pad to float64le alignment
+  coordinates_t coordinatesX; // Master coordinate vector
+  deltas_t deltasX; // Delta coordinate matrix
+  public:
+  DEFINE_SIZE_MIN (8);
 };
 
 } // namespace hvgl
