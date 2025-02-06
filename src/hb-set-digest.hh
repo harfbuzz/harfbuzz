4818ba9ece6df9d03d967e8c0cd6a8c001f4fa5a
Behdad Esfahbod <behdad@behdad.org>
2025-02-06 01:50:12 +0000

    [set-digest] Yet another tuning
    Saves 5% on SF Pro while regressing 2% on Roboto.

--- src/hb-set-digest.hh
+++ src/hb-set-digest.hh
@@ -69,7 +69,7 @@ struct hb_set_digest_t
   // No science in these. Intuition and testing only.
   using mask_t = uint64_t;
   static constexpr unsigned n = 3;
-#define HB_SET_DIGEST_SHIFTS {3, 0, 6}
+#define HB_SET_DIGEST_SHIFTS {4, 0, 6}
 
   static constexpr unsigned mask_bytes = sizeof (mask_t);
   static constexpr unsigned mask_bits = sizeof (mask_t) * 8;
