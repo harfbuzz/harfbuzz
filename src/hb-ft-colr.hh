17a83b748ef0ab029c63734d0407867c9f0cede9
Behdad Esfahbod <behdad@behdad.org>
2025-02-26 14:40:02 -0700

    [COLRv1] Optimize PaintColrLayers instead of PaintComposite
    See https://github.com/googlefonts/colr-gradients-spec/issues/369#issuecomment-1816913943
    and onwards.

--- src/hb-ft-colr.hh
+++ src/hb-ft-colr.hh
@@ -229,9 +229,7 @@ _hb_ft_paint (hb_ft_paint_context_t *c,
 	if (unlikely (!node.visit ((uintptr_t) other_paint.p)))
 	  continue;
 
-	c->funcs->push_group (c->data);
 	c->recurse (other_paint);
-	c->funcs->pop_group (c->data, HB_PAINT_COMPOSITE_MODE_SRC_OVER);
       }
     }
     break;
@@ -451,10 +449,12 @@ _hb_ft_paint (hb_ft_paint_context_t *c,
     break;
     case FT_COLR_PAINTFORMAT_COMPOSITE:
     {
+      c->funcs->push_group (c->data);
       c->recurse (paint.u.composite.backdrop_paint);
       c->funcs->push_group (c->data);
       c->recurse (paint.u.composite.source_paint);
       c->funcs->pop_group (c->data, _hb_ft_paint_composite_mode (paint.u.composite.composite_mode));
+      c->funcs->pop_group (c->data, HB_PAINT_COMPOSITE_MODE_SRC_OVER);
     }
     break;
 
