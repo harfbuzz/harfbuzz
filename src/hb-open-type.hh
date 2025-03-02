harfbuzz/harfbuzz

commit 043980a60eb2fe93dd65b8c2f5eaa021fd8653f2

[TupleValues] Micro-optimize

src/hb-open-type.hh
@@ -1914,6 +1914,7 @@ struct TupleValues
     void _add_to (hb_array_t<float> out, float scale = 1.0f)
     {
       unsigned n = out.length;
+      float *arrayZ = out.arrayZ;
 
       for (unsigned i = 0; i < n;)
       {
@@ -1926,21 +1927,21 @@ struct TupleValues
 	  {
 	    const auto *pp = (const HBINT8 *) p;
 	    for (unsigned j = 0; j < count; j++)
-	      out.arrayZ[i + j] += scaled ? *pp++ * scale : *pp++;
+	      *arrayZ++ += scaled ? *pp++ * scale : *pp++;
 	  }
 	  break;
 	  case 2:
 	  {
 	    const auto *pp = (const HBINT16 *) p;
 	    for (unsigned j = 0; j < count; j++)
-	      out.arrayZ[i + j] += scaled ? *pp++ * scale : *pp++;
+	      *arrayZ++ += scaled ? *pp++ * scale : *pp++;
 	  }
 	  break;
 	  case 4:
 	  {
 	    const auto *pp = (const HBINT32 *) p;
 	    for (unsigned j = 0; j < count; j++)
-	      out.arrayZ[i + j] += scaled ? *pp++ * scale : *pp++;
+	      *arrayZ++ += scaled ? *pp++ * scale : *pp++;
 	  }
 	  break;
 	}

commit d1826ca6264ecf10ff9ecb508d2ee071e0e13e01

[TupleValues] Fix regression
Introduced in 043980a60eb2fe93dd65b8c2f5eaa021fd8653f2

src/hb-open-type.hh
@@ -1943,6 +1943,11 @@ struct TupleValues
 	unsigned count = hb_min (n - i, (unsigned) run_count);
 	switch (width)
 	{
+	  case 0:
+	  {
+	    arrayZ += count;
+	    break;
+	  }
 	  case 1:
 	  {
 	    const auto *pp = (const HBINT8 *) p;