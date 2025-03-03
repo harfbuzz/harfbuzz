ff5404d04777c44c07a17fb15508abad6a4649a4
Behdad Esfahbod <behdad@behdad.org>
2025-03-03 11:48:00 -0700

    [hvgl] Fix sign of returned extents

--- src/hb-aat-var-hvgl-table.hh
+++ src/hb-aat-var-hvgl-table.hh
@@ -657,7 +657,7 @@ struct hvgl
       release_scratch (scratch);
 
       if (ret)
-	*extents = f_extents.to_glyph_extents ();
+	*extents = f_extents.to_glyph_extents (font->x_scale < 0, font->y_scale < 0);
 
       return ret;
     }
