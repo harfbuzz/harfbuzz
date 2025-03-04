d22e0d550742ea19cdf25ad8cbddfa517ec80a02
Behdad Esfahbod <behdad@behdad.org>
2025-03-04 02:28:51 -0700

    [fontations] Try folding into libharfbuzz

--- src/hb-fontations.cc
+++
@@ -1,9 +0,0 @@
-#include "hb-fontations.h"
-
-extern "C" void _hb_fontations_font_set_funcs (hb_font_t *font);
-
-void
-hb_fontations_font_set_funcs (hb_font_t *font)
-{
-  _hb_fontations_font_set_funcs (font);
-}
