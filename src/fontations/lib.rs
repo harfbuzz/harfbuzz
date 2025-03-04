26da19f6ea597a46473a317efe7b4d6e0518b256
Behdad Esfahbod <behdad@behdad.org>
2025-03-03 23:36:33 -0700

    Kinda builds now with bindgen
    Requires nightly.

--- src/fontations/lib.rs
+++ src/fontations/lib.rs
@@ -1,59 +1,13 @@
-//
-// lib.rs — Rust code that sets up HarfBuzz font funcs
-//
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+include!(concat!(env!("BUILD_ROOT"), "/src", "/hb.rs"));
 
-use std::os::raw::{c_void, c_char};
+use std::os::raw::{c_void};
 use std::ptr::{null_mut};
 
 // If you want to parse TTF/OTF with read-fonts, etc. import them:
 // use read_fonts::FontRef;
 // use skrifa::{...};
-
-// Minimal HarfBuzz FFI
-// Typically you'd generate these with bindgen from <hb.h>, but we'll do a small subset:
-
-pub type hb_bool_t = i32;
-pub type hb_position_t = i32;
-pub type hb_codepoint_t = u32;
-pub type hb_destroy_func_t = Option<extern "C" fn(user_data: *mut c_void)>;
-
-#[repr(C)]
-pub struct hb_font_t { _unused: [u8; 0], }
-
-#[repr(C)]
-pub struct hb_font_funcs_t { _unused: [u8; 0], }
-
-extern "C" {
-    fn hb_font_funcs_create() -> *mut hb_font_funcs_t;
-
-    fn hb_font_funcs_set_glyph_h_advance_func(
-        ffuncs: *mut hb_font_funcs_t,
-        func: Option<hb_font_get_glyph_advance_func_t>,
-        user_data: *mut c_void,
-        destroy: hb_destroy_func_t
-    );
-
-    fn hb_font_set_funcs(
-        font: *mut hb_font_t,
-        klass: *mut hb_font_funcs_t,
-        font_data: *mut c_void,
-        destroy: hb_destroy_func_t
-    );
-
-    fn hb_font_funcs_destroy (
-        ffuncs: *mut hb_font_funcs_t
-    );
-
-    // If you need more sets or functions, declare them here
-}
-
-// Callback type: e.g. horizontal advance
-pub type hb_font_get_glyph_advance_func_t = extern "C" fn(
-    font: *mut hb_font_t,
-    font_data: *mut c_void,
-    glyph: hb_codepoint_t,
-    user_data: *mut c_void
-) -> hb_position_t;
 
 // A struct for storing your “fontations” data
 #[repr(C)]
