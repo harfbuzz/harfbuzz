e052e0415e36e8ed95c9c0eb5e19ada2fa250bc3
Khaled Hosny <khaled@aliftype.com>
2025-03-07 02:53:43 +0200

    [directwrite] Fix applying features to directwrite shaper
    The range_features was ending up with random data because
    hb_ms_make_feature_ranges() keeps pointers to data allocated in
    feature_records and range_records, but that data was getting freed
    before range_features was used. Change the variable scope to avoid
    freeing the data too early.
    
    Fixes https://github.com/harfbuzz/harfbuzz/issues/5113

--- src/hb-directwrite.cc
+++ src/hb-directwrite.cc
@@ -637,11 +637,13 @@ _hb_directwrite_shape (hb_shape_plan_t    *shape_plan,
   static_assert ((sizeof (DWRITE_FONT_FEATURE) == sizeof (hb_ms_feature_t)), "");
   hb_vector_t<hb_ms_features_t *> range_features;
   hb_vector_t<uint32_t> range_char_counts;
+
+  hb_vector_t<hb_ms_feature_t> feature_records;
+  hb_vector_t<hb_ms_range_record_t> range_records;
   if (num_features)
   {
-    hb_vector_t<hb_ms_feature_t> feature_records;
-    hb_vector_t<hb_ms_range_record_t> range_records;
     if (hb_ms_setup_features (features, num_features, feature_records, range_records))
+    {
       hb_ms_make_feature_ranges (feature_records,
 				 range_records,
 				 0,
@@ -649,6 +651,7 @@ _hb_directwrite_shape (hb_shape_plan_t    *shape_plan,
 				 log_clusters,
 				 range_features,
 				 range_char_counts);
+    }
   }
 
   uint16_t* clusterMap;
