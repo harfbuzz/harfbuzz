fbb81e344b3ddba349d1e2cefa2ff42515aef51d
Behdad Esfahbod <behdad@behdad.org>
2025-03-12 00:52:12 -0600

    [fontations] Adjust alignment requirement

--- src/fontations/lib.rs
+++ src/fontations/lib.rs
@@ -28,22 +28,22 @@ struct MyAllocator;
 
 unsafe impl GlobalAlloc for MyAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        assert!(layout.align() <= 16);
+        assert!(layout.align() <= 2 * std::mem::size_of::<*mut u8>());
         hb_malloc(layout.size()) as *mut u8
     }
 
     unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        assert!(layout.align() <= 16);
+        assert!(layout.align() <= 2 * std::mem::size_of::<*mut u8>());
         hb_calloc(layout.size(), 1) as *mut u8
     }
 
     unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        assert!(layout.align() <= 16);
+        assert!(layout.align() <= 2 * std::mem::size_of::<*mut u8>());
         hb_realloc(ptr as *mut c_void, new_size) as *mut u8
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        assert!(layout.align() <= 16);
+        assert!(layout.align() <= 2 * std::mem::size_of::<*mut u8>());
         hb_free(ptr as *mut c_void);
     }
 }
