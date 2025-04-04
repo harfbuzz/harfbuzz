7ba4e879349c0aaa16697bafb52aafb4b67037bd
Behdad Esfahbod <behdad@behdad.org>
2025-04-04 12:39:10 -0600

    [VarStore] Fix storage shift
    Use all bits. That's what I meant.

--- src/hb-ot-layout-common.hh
+++ src/hb-ot-layout-common.hh
@@ -2548,9 +2548,9 @@ struct SparseVarRegionAxis
   DEFINE_SIZE_STATIC (8);
 };
 
-#define REGION_CACHE_ITEM_CACHE_INVALID (1 << 30)
-#define REGION_CACHE_ITEM_MULTIPLIER (float (1 << 19))
-#define REGION_CACHE_ITEM_DIVISOR (1.f / float (1 << 19))
+#define REGION_CACHE_ITEM_CACHE_INVALID (-1 << 31)
+#define REGION_CACHE_ITEM_MULTIPLIER (float (1 << 30))
+#define REGION_CACHE_ITEM_DIVISOR (1.f / float (1 << 30))
 
 struct VarRegionList
 {
