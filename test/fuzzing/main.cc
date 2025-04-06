e33277776363803c53bd1b456ffb9d4f653ab897
Behdad Esfahbod <behdad@behdad.org>
2025-04-06 02:30:09 -0600

    [fuzzing/subset] Make TAP protocol

--- test/fuzzing/main.cc
+++ test/fuzzing/main.cc
@@ -5,6 +5,8 @@
 
 int main (int argc, char **argv)
 {
+  printf ("TAP version 14\n");
+
   for (int i = 1; i < argc; i++)
   {
     hb_blob_t *blob = hb_blob_create_from_file_or_fail (argv[i]);
@@ -12,11 +14,16 @@ int main (int argc, char **argv)
 
     unsigned len = 0;
     const char *font_data = hb_blob_get_data (blob, &len);
-    printf ("%s (%u bytes)\n", argv[i], len);
+    printf ("# %s (%u bytes)\n", argv[i], len);
 
     LLVMFuzzerTestOneInput ((const uint8_t *) font_data, len);
 
+    printf ("ok %d - %s\n", i, argv[i]);
+
     hb_blob_destroy (blob);
   }
+
+  printf ("1..%d\n", argc - 1);
+
   return 0;
 }
