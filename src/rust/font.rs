71523a421e194ad15cf032b6593163348445bdf7
Behdad Esfahbod <behdad@behdad.org>
2025-05-05 12:09:47 -0600

    [fontations] Move code from fontations/lib.rs into rust/font.rs
    Add a rust feature for enable font backend.
    
    In anticipation of adding a shaper using harfRuzz.

--- src/rust/font.rs
+++ src/rust/font.rs
@@ -1,5 +1,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
+#![allow(dead_code)]
 include!(concat!(env!("OUT_DIR"), "/hb.rs"));
 
 use std::alloc::{GlobalAlloc, Layout};
---
+++ src/rust/lib.rs
@@ -0,0 +1,4 @@
+#[cfg(feature = "font")]
+mod font;
+#[cfg(feature = "font")]
+pub use font::hb_fontations_font_set_funcs;
