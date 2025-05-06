d403529211eb1659ec3de3260ed30a64a301f59e
Khaled Hosny <khaled@aliftype.com>
2025-05-06 12:33:33 +0300

    [check-*.py] Format

--- src/check-libstdc++.py
+++ src/check-libstdc++.py
@@ -1,39 +1,43 @@
 #!/usr/bin/env python3
 
-import sys, os, shutil, subprocess
+import os
+import shutil
+import subprocess
+import sys
 
-os.chdir (os.getenv ('srcdir', os.path.dirname (__file__)))
+os.chdir(os.getenv("srcdir", os.path.dirname(__file__)))
 
-libs = os.getenv ('libs', '.libs')
+libs = os.getenv("libs", ".libs")
 
-ldd = os.getenv ('LDD', shutil.which ('ldd'))
+ldd = os.getenv("LDD", shutil.which("ldd"))
 if not ldd:
-	otool = os.getenv ('OTOOL', shutil.which ('otool'))
-	if otool:
-		ldd = otool + ' -L'
-	else:
-		print ('check-libstdc++.py: \'ldd\' not found; skipping test')
-		sys.exit (77)
+    otool = os.getenv("OTOOL", shutil.which("otool"))
+    if otool:
+        ldd = otool + " -L"
+    else:
+        print("check-libstdc++.py: 'ldd' not found; skipping test")
+        sys.exit(77)
 
 stat = 0
 tested = False
 
 # harfbuzz-icu links to libstdc++ because icu does.
-for soname in ['harfbuzz', 'harfbuzz-subset', 'harfbuzz-gobject', 'harfbuzz-cairo']:
-	for suffix in ['so', 'dylib']:
-		so = os.path.join (libs, 'lib%s.%s' % (soname, suffix))
-		if not os.path.exists (so): continue
+for soname in ["harfbuzz", "harfbuzz-subset", "harfbuzz-gobject", "harfbuzz-cairo"]:
+    for suffix in ["so", "dylib"]:
+        so = os.path.join(libs, "lib%s.%s" % (soname, suffix))
+        if not os.path.exists(so):
+            continue
 
-		print ('Checking that we are not linking to libstdc++ or libc++ in %s' % so)
-		ldd_result = subprocess.check_output (ldd.split() + [so])
-		if (b'libstdc++' in ldd_result) or (b'libc++' in ldd_result):
-			print ('Ouch, %s is linked to libstdc++ or libc++' % so)
-			stat = 1
+        print("Checking that we are not linking to libstdc++ or libc++ in %s" % so)
+        ldd_result = subprocess.check_output(ldd.split() + [so])
+        if (b"libstdc++" in ldd_result) or (b"libc++" in ldd_result):
+            print("Ouch, %s is linked to libstdc++ or libc++" % so)
+            stat = 1
 
-		tested = True
+        tested = True
 
 if not tested:
-	print ('check-libstdc++.py: libharfbuzz shared library not found; skipping test')
-	sys.exit (77)
+    print("check-libstdc++.py: libharfbuzz shared library not found; skipping test")
+    sys.exit(77)
 
-sys.exit (stat)
+sys.exit(stat)
--- src/check-static-inits.py
+++ src/check-static-inits.py
@@ -1,51 +1,60 @@
 #!/usr/bin/env python3
 
-import sys, os, shutil, subprocess, glob, re
+import glob
+import os
+import re
+import shutil
+import subprocess
+import sys
 
-builddir = os.getenv ('builddir', os.path.dirname (__file__))
-libs = os.getenv ('libs', '.libs')
+builddir = os.getenv("builddir", os.path.dirname(__file__))
+libs = os.getenv("libs", ".libs")
 
-objdump = os.getenv ('OBJDUMP', shutil.which ('objdump'))
+objdump = os.getenv("OBJDUMP", shutil.which("objdump"))
 if not objdump:
-	print ('check-static-inits.py: \'ldd\' not found; skipping test')
-	sys.exit (77)
+    print("check-static-inits.py: 'ldd' not found; skipping test")
+    sys.exit(77)
 
 if sys.version_info < (3, 5):
-	print ('check-static-inits.py: needs python 3.5 for recursive support in glob')
-	sys.exit (77)
+    print("check-static-inits.py: needs python 3.5 for recursive support in glob")
+    sys.exit(77)
 
-OBJS = glob.glob (os.path.join (builddir, libs, '**', '*hb*.o'), recursive=True)
+OBJS = glob.glob(os.path.join(builddir, libs, "**", "*hb*.o"), recursive=True)
 if not OBJS:
-	print ('check-static-inits.py: object files not found; skipping test')
-	sys.exit (77)
+    print("check-static-inits.py: object files not found; skipping test")
+    sys.exit(77)
 
 stat = 0
 tested = 0
 
-result = subprocess.run(objdump.split () + ['-t'] + OBJS, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+result = subprocess.run(
+    objdump.split() + ["-t"] + OBJS, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+)
 
 if result.returncode:
-    if result.stderr.find (b'not recognized') != -1:
+    if result.stderr.find(b"not recognized") != -1:
         # https://github.com/harfbuzz/harfbuzz/issues/3019
-        print ('objdump %s returned "not recognized", skipping')
+        print('objdump %s returned "not recognized", skipping')
     else:
-        print ('objdump returned error:\n%s' % (result.stderr.decode ('utf-8')))
+        print("objdump returned error:\n%s" % (result.stderr.decode("utf-8")))
         stat = 2
 else:
     tested = 1
 
-result = result.stdout.decode ('utf-8')
+result = result.stdout.decode("utf-8")
 
 # Checking that no object file has static initializers
-for l in re.findall (r'^.*\.[cd]tors.*$', result, re.MULTILINE):
-    if not re.match (r'.*\b0+\b', l):
-        print ('Ouch, library has static initializers/finalizers')
+for l in re.findall(r"^.*\.[cd]tors.*$", result, re.MULTILINE):
+    if not re.match(r".*\b0+\b", l):
+        print("Ouch, library has static initializers/finalizers")
         stat = 1
 
 # Checking that no object file has lazy static C++ constructors/destructors or other such stuff
-if ('__cxa_' in result) and ('__ubsan_handle' not in result):
-    print ('Ouch, library has lazy static C++ constructors/destructors or other such stuff')
+if ("__cxa_" in result) and ("__ubsan_handle" not in result):
+    print(
+        "Ouch, library has lazy static C++ constructors/destructors or other such stuff"
+    )
     stat = 1
 
 
-sys.exit (stat if tested else 77)
+sys.exit(stat if tested else 77)
