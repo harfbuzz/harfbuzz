5060ccf4aa107dc9aa8e6a691755d4afe4f94744
Behdad Esfahbod <behdad@behdad.org>
2025-06-11 16:01:54 -0600

    [rust] Ignore one more compiler symbol

--- src/check-symbols.py
+++ src/check-symbols.py
@@ -42,6 +42,7 @@ IGNORED_SYMBOLS += [
     "_ZN3std3sys3pal4unix4args3imp15ARGV_INIT_ARRAY.*",  # 'std::sys::pal::unix::args::imp::ARGV_INIT_ARRAY::.*'
     "_ZN3std3sys4args4unix3imp15ARGV_INIT_ARRAY.*",  # std::sys::args::unix::imp::ARGV_INIT_ARRAY::.*
     "_ZN17compiler_builtins.*",  # 'compiler_builtins::.*'
+    ".*__rustc.*",  # '.*__rustc.*' # Eg. _RNvCsgSLETaxrkfn_7___rustc17___rust_probestack
     '_hb_harfrust_.*_rs',
 ]
 
