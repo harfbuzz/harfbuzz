64bcc95ce5c4e4ae8151b60febc685312dc65911
Behdad Esfahbod <behdad@behdad.org>
2025-06-12 10:08:20 -0600

    Comment

--- src/hb-ot-font.cc
+++ src/hb-ot-font.cc
@@ -565,10 +565,10 @@ hb_ot_draw_glyph_or_fail (hb_font_t *font,
 #ifndef HB_NO_VAR_COMPOSITES
   if (font->face->table.VARC->get_path (font, glyph, draw_session)) { ret = true; goto done; }
 #endif
+  // Keep the following in synch with VARC::get_path_at()
 #ifndef HB_NO_VAR_HVF
   if (font->face->table.hvgl->get_path (font, glyph, draw_session)) { ret = true; goto done; }
 #endif
-  // Keep the following in synch with VARC::get_path_at()
   if (font->face->table.glyf->get_path (font, glyph, draw_session, gvar_cache)) { ret = true; goto done; }
 
 #ifndef HB_NO_CFF
