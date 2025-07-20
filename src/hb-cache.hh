4214bfef55fc4ce513c17cd7084d2c4487003851
Behdad Esfahbod <behdad@behdad.org>
2025-07-20 06:55:56 -0600

    [cache] Simplify return

--- src/hb-cache.hh
+++ src/hb-cache.hh
@@ -94,14 +94,13 @@ struct hb_cache_t
     return true;
   }
 
-  bool set (unsigned int key, unsigned int value)
+  void set (unsigned int key, unsigned int value)
   {
     if (unlikely ((key >> key_bits) || (value >> value_bits)))
-      return false; /* Overflows */
+      return; /* Overflows */
     unsigned int k = key & ((1u<<cache_bits)-1);
     unsigned int v = ((key>>cache_bits)<<value_bits) | value;
     values[k] = v;
-    return true;
   }
 
   private:
