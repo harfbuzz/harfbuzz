f63d960c4eda79514c060ddce796df9e607b5bae
Behdad Esfahbod <behdad@behdad.org>
2025-07-22 16:59:48 -0600

    Fix a MSVC warning re __has_builtin
    Fixes https://github.com/harfbuzz/harfbuzz/issues/5395

--- src/hb.hh
+++ src/hb.hh
@@ -239,6 +239,8 @@
 // clang defines it so no need.
 #ifdef __has_builtin
 #define hb_has_builtin __has_builtin
+#elif defined(_MSC_VER)
+#define hb_has_builtin(x) 0
 #else
 #define hb_has_builtin(x) ((defined(__GNUC__) && __GNUC__ >= 5))
 #endif
