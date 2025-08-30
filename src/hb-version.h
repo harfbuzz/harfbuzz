22f77d48abc35c1e053d02f1ada0d8f1ce1b8423
Khaled Hosny <khaled@aliftype.com>
2025-08-30 11:39:01 +0300

    11.4.5

--- src/hb-version.h
+++ src/hb-version.h
@@ -53,14 +53,14 @@ HB_BEGIN_DECLS
  *
  * The micro component of the library version available at compile-time.
  */
-#define HB_VERSION_MICRO 4
+#define HB_VERSION_MICRO 5
 
 /**
  * HB_VERSION_STRING:
  *
  * A string literal containing the library version available at compile-time.
  */
-#define HB_VERSION_STRING "11.4.4"
+#define HB_VERSION_STRING "11.4.5"
 
 /**
  * HB_VERSION_ATLEAST:
