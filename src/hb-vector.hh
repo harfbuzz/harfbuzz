fb0ef230c98b527ca584ea059978aa909a5a052a
Behdad Esfahbod <behdad@behdad.org>
2025-09-16 11:16:30 -0600

    Revert "[tuplevars] Allocate from pool"
    This reverts commit 94d78de954ab22f3717cc399920670f877616567.
    
    Slowed things down.

--- src/hb-vector.hh
+++ src/hb-vector.hh
@@ -107,6 +107,9 @@ struct hb_vector_t
   void
   set_storage (Type *array, unsigned n)
   {
+    if (unlikely (in_error ()))
+      return;
+
     assert (allocated == 0);
     assert (length == 0);
 
@@ -554,10 +557,12 @@ struct hb_vector_t
 	set_error ();
 	return false;
       }
+      set_storage (arrayZ, size);
       if (initialize)
+      {
+	length = 0;
 	grow_vector (size, hb_prioritize);
-      else
-        length = size;
+      }
       return true;
     }
     return resize_full ((int) size, initialize, true);
