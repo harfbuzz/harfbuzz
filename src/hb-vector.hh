09c14fa4f28b48ae9566c3aa3a145ebd124093cd
Garret Rieger <grieger@google.com>
2025-11-14 18:27:35 +0900

    Fix fuzzer found memory leak. (#5643)
    Fixes https://oss-fuzz.com/testcase-detail/6083012753424384

--- src/hb-vector.hh
+++ src/hb-vector.hh
@@ -313,6 +313,11 @@ struct hb_vector_t
     assert (allocated < 0);
     allocated = -(allocated + 1);
   }
+  void ensure_error ()
+  {
+    if (!in_error ())
+      set_error ();
+  }
 
   Type *
   _realloc (unsigned new_allocated)
