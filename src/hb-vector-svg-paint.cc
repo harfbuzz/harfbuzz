5870854182b9ee8b3e38984cf2c9a70a0da8b348
Behdad Esfahbod <behdad@behdad.org>
2026-02-24 20:06:56 -0300

    [vector] Remove leftover cross-context structs after split

--- src/hb-vector-svg-paint.cc
+++ src/hb-vector-svg-paint.cc
@@ -429,35 +429,6 @@ hb_svg_set_glyph_extents_common (const hb_transform_t<> &transform,
 }
 
 
-struct hb_vector_draw_t
-{
-  hb_object_header_t header;
-
-  hb_vector_format_t format = HB_VECTOR_FORMAT_SVG;
-  hb_transform_t<> transform = {1, 0, 0, 1, 0, 0};
-  float x_scale_factor = 1.f;
-  float y_scale_factor = 1.f;
-  hb_vector_extents_t extents = {0, 0, 0, 0};
-  bool has_extents = false;
-  unsigned precision = 2;
-  bool flat = false;
-
-  hb_vector_t<char> defs;
-  hb_vector_t<char> body;
-  hb_vector_t<char> path;
-  hb_set_t *defined_glyphs = nullptr;
-  hb_blob_t *recycled_blob = nullptr;
-
-  void append_xy (float x, float y)
-  {
-    float tx, ty;
-    hb_svg_transform_point (transform, x_scale_factor, y_scale_factor, x, y, &tx, &ty);
-    hb_svg_append_num (&path, tx, precision);
-    hb_svg_append_c (&path, ',');
-    hb_svg_append_num (&path, ty, precision);
-  }
-};
-
 struct hb_svg_color_glyph_cache_key_t
 {
   hb_codepoint_t glyph = HB_CODEPOINT_INVALID;
