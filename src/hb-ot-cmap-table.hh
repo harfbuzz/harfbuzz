965fe2fa58b6435774ada15887cc2569975beef4
Behdad Esfahbod <behdad@behdad.org>
2026-03-02 01:56:54 -0300

    Clean up Arabic PUA packtab symbols

--- src/hb-ot-cmap-table.hh
+++ src/hb-ot-cmap-table.hh
@@ -1769,7 +1769,7 @@ struct SubtableUnicodesCache {
 
 };
 
-static inline uint_fast16_t
+static inline uint16_t
 _hb_symbol_pua_map (unsigned codepoint)
 {
   if (codepoint <= 0x00FFu)
@@ -2236,7 +2236,7 @@ struct cmap
     typedef bool (*hb_cmap_get_glyph_func_t) (const void *obj,
 					      hb_codepoint_t codepoint,
 					      hb_codepoint_t *glyph);
-    typedef uint_fast16_t (*hb_pua_remap_func_t) (unsigned);
+    typedef uint16_t (*hb_pua_remap_func_t) (unsigned);
 
     template <typename Type>
     HB_INTERNAL static bool get_glyph_from (const void *obj,
