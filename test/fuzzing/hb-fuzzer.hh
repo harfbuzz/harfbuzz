c4ef89c63d810a7bf23751c287c01da00320b3d5
Behdad Esfahbod <behdad@behdad.org>
2026-03-04 13:44:18 -0300

    Add extended hb-subset fuzzer seed recorder

--- test/fuzzing/hb-fuzzer.hh
+++ test/fuzzing/hb-fuzzer.hh
@@ -2,6 +2,7 @@
 
 #include <hb.h>
 #include <stddef.h>
+#include <string.h>
 
 extern "C" int LLVMFuzzerTestOneInput (const uint8_t *data, size_t size);
 
@@ -33,4 +34,82 @@ _fuzzing_alloc_state (const uint8_t *data, size_t size)
     return 0;
 
   return size;
+}
+
+static const uint8_t _fuzzing_extended_magic[] = {'H', 'B', 'S', 'U', 'B', 'F', 'Z', '2'};
+
+enum _fuzzing_extended_op_t
+{
+  HB_FUZZING_OP_SET_FLAGS = 1,
+  HB_FUZZING_OP_KEEP_EVERYTHING = 2,
+  HB_FUZZING_OP_SET_CLEAR = 3,
+  HB_FUZZING_OP_SET_INVERT = 4,
+  HB_FUZZING_OP_SET_ADD_RANGES = 5,
+  HB_FUZZING_OP_SET_DEL_RANGES = 6,
+  HB_FUZZING_OP_TEXT_ADD = 7,
+  HB_FUZZING_OP_TEXT_DEL = 8,
+  HB_FUZZING_OP_AXIS_PIN_ALL_TO_DEFAULT = 9,
+  HB_FUZZING_OP_AXIS_SET = 10,
+};
+
+enum _fuzzing_extended_axis_mode_t
+{
+  HB_FUZZING_AXIS_PIN_TO_DEFAULT = 0,
+  HB_FUZZING_AXIS_SET_RANGE = 1,
+};
+
+static inline uint32_t
+_fuzzing_read_u32_le (const uint8_t *p)
+{
+  return (uint32_t) p[0] |
+         ((uint32_t) p[1] << 8) |
+         ((uint32_t) p[2] << 16) |
+         ((uint32_t) p[3] << 24);
+}
+
+static inline float
+_fuzzing_read_f32_le (const uint8_t *p)
+{
+  uint32_t bits = _fuzzing_read_u32_le (p);
+  float value;
+  memcpy (&value, &bits, sizeof (value));
+  return value;
+}
+
+template <typename T>
+static inline bool
+_fuzzing_read_value (const uint8_t *&p,
+                     const uint8_t *end,
+                     T *out)
+{
+  if ((size_t) (end - p) < sizeof (T))
+    return false;
+
+  memcpy (out, p, sizeof (T));
+  p += sizeof (T);
+  return true;
+}
+
+static inline bool
+_fuzzing_read_u32_value (const uint8_t *&p,
+                         const uint8_t *end,
+                         uint32_t *out)
+{
+  if ((size_t) (end - p) < 4)
+    return false;
+  *out = _fuzzing_read_u32_le (p);
+  p += 4;
+  return true;
+}
+
+static inline bool
+_fuzzing_read_f32_value (const uint8_t *&p,
+                         const uint8_t *end,
+                         float *out)
+{
+  if ((size_t) (end - p) < 4)
+    return false;
+  *out = _fuzzing_read_f32_le (p);
+  p += 4;
+  return true;
 }
--- test/fuzzing/hb-subset-fuzzer.cc
+++ test/fuzzing/hb-subset-fuzzer.cc
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <assert.h>
 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
 #include "hb-subset.h"
 
 static void
@@ -44,10 +48,222 @@ trySubset (hb_face_t *face,
   hb_subset_input_destroy (input);
 }
 
-extern "C" int LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
+static bool
+read_ranges (const uint8_t *&p,
+             const uint8_t *end,
+             hb_set_t *set,
+             bool is_add)
 {
-  alloc_state = _fuzzing_alloc_state (data, size);
+  uint32_t count;
+  if (!_fuzzing_read_u32_value (p, end, &count))
+    return false;
 
+  for (uint32_t i = 0; i < count; i++)
+  {
+    uint32_t start, finish;
+    if (!_fuzzing_read_u32_value (p, end, &start) ||
+        !_fuzzing_read_u32_value (p, end, &finish))
+      return false;
+
+    if (finish < start)
+      return false;
+
+    if (is_add)
+      hb_set_add_range (set, start, finish);
+    else
+      hb_set_del_range (set, start, finish);
+  }
+  return true;
+}
+
+static bool
+apply_extended_ops (hb_face_t *face,
+                    hb_subset_input_t *input,
+                    std::vector<hb_codepoint_t> *text,
+                    unsigned *flags,
+                    const uint8_t *ops,
+                    size_t ops_len)
+{
+  const uint8_t *p = ops;
+  const uint8_t *end = ops + ops_len;
+
+  while (p < end)
+  {
+    uint8_t op;
+    if (!_fuzzing_read_value (p, end, &op))
+      return false;
+
+    switch (op)
+    {
+      case HB_FUZZING_OP_SET_FLAGS:
+      {
+        uint32_t value;
+        if (!_fuzzing_read_u32_value (p, end, &value))
+          return false;
+        *flags = value;
+        break;
+      }
+
+      case HB_FUZZING_OP_KEEP_EVERYTHING:
+        hb_subset_input_keep_everything (input);
+        *flags = hb_subset_input_get_flags (input);
+        break;
+
+      case HB_FUZZING_OP_SET_CLEAR:
+      case HB_FUZZING_OP_SET_INVERT:
+      {
+        uint8_t set_type_u8;
+        if (!_fuzzing_read_value (p, end, &set_type_u8))
+          return false;
+        if (set_type_u8 > HB_SUBSET_SETS_LAYOUT_SCRIPT_TAG)
+          return false;
+
+        hb_set_t *set = hb_subset_input_set (input, (hb_subset_sets_t) set_type_u8);
+        if (op == HB_FUZZING_OP_SET_CLEAR)
+          hb_set_clear (set);
+        else
+          hb_set_invert (set);
+        break;
+      }
+
+      case HB_FUZZING_OP_SET_ADD_RANGES:
+      case HB_FUZZING_OP_SET_DEL_RANGES:
+      {
+        uint8_t set_type_u8;
+        if (!_fuzzing_read_value (p, end, &set_type_u8))
+          return false;
+        if (set_type_u8 > HB_SUBSET_SETS_LAYOUT_SCRIPT_TAG)
+          return false;
+
+        hb_set_t *set = hb_subset_input_set (input, (hb_subset_sets_t) set_type_u8);
+        if (!read_ranges (p, end, set, op == HB_FUZZING_OP_SET_ADD_RANGES))
+          return false;
+        break;
+      }
+
+      case HB_FUZZING_OP_TEXT_ADD:
+      case HB_FUZZING_OP_TEXT_DEL:
+      {
+        uint32_t count;
+        if (!_fuzzing_read_u32_value (p, end, &count))
+          return false;
+
+        for (uint32_t i = 0; i < count; i++)
+        {
+          uint32_t cp;
+          if (!_fuzzing_read_u32_value (p, end, &cp))
+            return false;
+          if (op == HB_FUZZING_OP_TEXT_ADD)
+            text->push_back (cp);
+          else
+            text->erase (std::remove (text->begin (), text->end (), cp), text->end ());
+        }
+        break;
+      }
+
+      case HB_FUZZING_OP_AXIS_PIN_ALL_TO_DEFAULT:
+        if (!hb_subset_input_pin_all_axes_to_default (input, face))
+          return false;
+        break;
+
+      case HB_FUZZING_OP_AXIS_SET:
+      {
+        uint32_t count;
+        if (!_fuzzing_read_u32_value (p, end, &count))
+          return false;
+
+        for (uint32_t i = 0; i < count; i++)
+        {
+          uint32_t tag;
+          uint8_t mode;
+          float minimum, middle, maximum;
+          if (!_fuzzing_read_u32_value (p, end, &tag) ||
+              !_fuzzing_read_value (p, end, &mode) ||
+              !_fuzzing_read_f32_value (p, end, &minimum) ||
+              !_fuzzing_read_f32_value (p, end, &middle) ||
+              !_fuzzing_read_f32_value (p, end, &maximum))
+            return false;
+
+          if (mode == HB_FUZZING_AXIS_PIN_TO_DEFAULT)
+          {
+            if (!hb_subset_input_pin_axis_to_default (input, face, tag))
+              return false;
+          }
+          else if (mode == HB_FUZZING_AXIS_SET_RANGE)
+          {
+            if (!hb_subset_input_set_axis_range (input, face, tag, minimum, maximum, middle))
+              return false;
+          }
+          else
+            return false;
+        }
+        break;
+      }
+
+      default:
+        return false;
+    }
+  }
+
+  return true;
+}
+
+static bool
+try_extended_input (const uint8_t *data, size_t size)
+{
+  if (size < sizeof (_fuzzing_extended_magic) + 4)
+    return false;
+
+  size_t magic_offset = size - sizeof (_fuzzing_extended_magic);
+  if (0 != memcmp (data + magic_offset, _fuzzing_extended_magic, sizeof (_fuzzing_extended_magic)))
+    return false;
+
+  size_t ops_len_offset = magic_offset - 4;
+  uint32_t ops_len = _fuzzing_read_u32_le (data + ops_len_offset);
+  if (ops_len > ops_len_offset)
+    return false;
+
+  size_t font_len = ops_len_offset - ops_len;
+  const uint8_t *ops = data + font_len;
+
+  hb_blob_t *blob = hb_blob_create ((const char *) data, font_len,
+				    HB_MEMORY_MODE_READONLY, nullptr, nullptr);
+  hb_face_t *face = hb_face_create (blob, 0);
+
+  hb_set_t *output = hb_set_create ();
+  hb_face_collect_unicodes (face, output);
+  hb_set_destroy (output);
+
+  hb_subset_input_t *input = hb_subset_input_create_or_fail ();
+  if (!input)
+  {
+    hb_face_destroy (face);
+    hb_blob_destroy (blob);
+    return true;
+  }
+
+  std::vector<hb_codepoint_t> text;
+  unsigned flags = HB_SUBSET_FLAGS_DEFAULT;
+  if (apply_extended_ops (face, input, &text, &flags, ops, ops_len))
+  {
+    trySubset (face,
+               text.empty () ? nullptr : text.data (),
+               (int) text.size (),
+               flags,
+               input);
+  }
+  else
+    hb_subset_input_destroy (input);
+
+  hb_face_destroy (face);
+  hb_blob_destroy (blob);
+
+  return true;
+}
+
+static void
+try_legacy_input (const uint8_t *data, size_t size)
+{
   hb_blob_t *blob = hb_blob_create ((const char *) data, size,
 				    HB_MEMORY_MODE_READONLY, nullptr, nullptr);
   hb_face_t *face = hb_face_create (blob, 0);
@@ -69,19 +285,19 @@ extern "C" int LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
   {
     hb_face_destroy (face);
     hb_blob_destroy (blob);
-    return 0;
+    return;
   }
   trySubset (face, text, sizeof (text) / sizeof (hb_codepoint_t), flags, input);
 
   unsigned num_axes;
   hb_codepoint_t text_from_data[16];
   if (size > sizeof (text_from_data) + sizeof (flags) + sizeof(num_axes)) {
-    hb_subset_input_t *input = hb_subset_input_create_or_fail ();
-    if (!input)
+    hb_subset_input_t *legacy_input = hb_subset_input_create_or_fail ();
+    if (!legacy_input)
     {
       hb_face_destroy (face);
       hb_blob_destroy (blob);
-      return 0;
+      return;
     }
     size -= sizeof (text_from_data);
     memcpy (text_from_data,
@@ -112,21 +328,28 @@ extern "C" int LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
                 data + size,
                 sizeof (value));
 
-        hb_subset_input_pin_axis_location(input,
+        hb_subset_input_pin_axis_location(legacy_input,
                                           face,
                                           tag,
                                           (float) value);
       }
     }
 
-
-
     unsigned int text_size = sizeof (text_from_data) / sizeof (hb_codepoint_t);
-    trySubset (face, text_from_data, text_size, flags, input);
+    trySubset (face, text_from_data, text_size, flags, legacy_input);
   }
 
   hb_face_destroy (face);
   hb_blob_destroy (blob);
+}
 
+extern "C" int LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
+{
+  alloc_state = _fuzzing_alloc_state (data, size);
+
+  if (try_extended_input (data, size))
+    return 0;
+
+  try_legacy_input (data, size);
   return 0;
 }
