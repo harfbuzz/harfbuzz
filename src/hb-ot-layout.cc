d8617e1e911a99915b3ffa72688f7eb338242295
Behdad Esfahbod <behdad@behdad.org>
2026-07-30 10:49:20 -0600

    Fix an API nullable crash
    Fixes https://github.com/harfbuzz/harfbuzz/issues/6112

--- src/hb-ot-layout.cc
+++ src/hb-ot-layout.cc
@@ -975,7 +975,7 @@ hb_ot_layout_language_get_feature_tags (hb_face_t    *face,
   static_assert ((sizeof (unsigned int) == sizeof (hb_tag_t)), "");
   unsigned int ret = l.get_feature_indexes (start_offset, feature_count, (unsigned int *) feature_tags);
 
-  if (feature_tags) {
+  if (feature_count && feature_tags) {
     unsigned int count = *feature_count;
     for (unsigned int i = 0; i < count; i++)
       feature_tags[i] = g.get_feature_tag ((unsigned int) feature_tags[i]);
