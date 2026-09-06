cdbe72ca8bf2c077e91ae10c4e428e1183d7d5ec
David Corbett <corbett.dav@northeastern.edu>
2026-09-06 14:55:21 -0400

    [USE] Update the data files for Unicode 18.0 (#6242)
    This uses the data files from
    <https://github.com/microsoft/font-tools/tree/9dcd14bcc9d9bb634b813fe06e1aaa2bc0209a7d/USE>.

--- src/gen-use-table.py
+++ src/gen-use-table.py
@@ -408,7 +408,7 @@ def map_to_use(data):
 		if U in [0x11302, 0x11303, 0x114C1]: UIPC = Top
 
 		assert (UIPC in [Not_Applicable, Visual_Order_Left] or
-			U in {0x0F7F, 0x11A3A} or
+			U == 0x0F7F or
 			USE in use_positions), "%s %s %s %s %s %s %s" % (hex(U), UIPC, USE, UISC, UDI, UGC, AJT)
 
 		pos_mapping = use_positions.get(USE, None)
